857642408d0c5fb449ad3ebbd3a4399cdf58584b
gilf <gil.fink@gmail.com>
2022-10-22 18:31:32 +0300

    Add swap.tsx component

--- packages/qwik-ui/src/index.ts
+++ packages/qwik-ui/src/index.ts
@@ -1,2 +1,4 @@
-export { Logo } from './components/logo/logo';
-export { Counter } from './components/counter/counter';
+export { Alert } from './components/alert/alert';
+export { Button } from './components/button/button';
+export { ButtonGroup } from './components/buttonGroup/buttonGroup';
+export { Swap } from './components/swap/swap';
