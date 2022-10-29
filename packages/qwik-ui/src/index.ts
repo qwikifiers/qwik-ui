0e23f14209fe62346f730621dddcead68656fec8
gilf <gil.fink@gmail.com>
2022-10-29 15:37:13 +0300

    Added divider.tsx component

--- packages/qwik-ui/src/index.ts
+++ packages/qwik-ui/src/index.ts
@@ -4,6 +4,7 @@ export { ButtonGroup } from './components/buttonGroup/buttonGroup';
 export { Breadcrumb, BreadcrumbItem } from './components/breadcrumb';
 export { Checkbox } from './components/checkbox/checkbox';
 export { Collapse, CollapseTitle, CollapseContent } from './components/collapse';
+export { Divider } from './components/divider/divider'
 export { Dropdown, DropdownItem, DropdownTrigger, DropdownContent } from './components/dropdown';
 export { Swap } from './components/swap/swap';
 export { Card, CardActions, CardContent } from './components/card';
