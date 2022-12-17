84b9cefb5b054727d8ed42de33d2e22c09364e80
gioboa <giorgiob.boa@gmail.com>
2022-12-17 16:59:03 +0100

    feat: add routes into website homepage

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -2,7 +2,15 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return <div>This will be a real website soon :)</div>;
+  return (
+    <div>
+      This will be a real website soon :)
+      <hr />
+      <a href="/headless-example">headless examples</a>
+      <hr />
+      <a href="/daisy-example">daisy examples</a>
+    </div>
+  );
 });
 
 export const head: DocumentHead = {
