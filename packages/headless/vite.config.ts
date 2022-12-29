4d7031f472190fa7ad262b2cbc700871fc0c76eb
Shai Reznik <shairez@users.noreply.github.com>
2022-12-29 12:46:20 +0200

    WIP: getting headless to release state
    Co-authored-by: Glenn Becker <nnelgxorz@gmail.com>

--- packages/headless/vite.config.ts
+++ packages/headless/vite.config.ts
@@ -20,11 +20,13 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    target: 'es2020',
     lib: {
+      entry: './src/index.ts',
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'limpy',
-      fileName: 'index',
+      name: 'headless',
+      fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
+      // fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
       formats: ['es', 'cjs'],
