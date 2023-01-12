8cdc382ad47a5486f7ee9172f1532571dab3e5c0
Shai Reznik <shairez@users.noreply.github.com>
2023-01-12 02:13:08 +0200

    chore(headless): add storybook, cypress and fix build
    fixed the build for the headless package, rest is still WIP

---
+++ packages/headless/.storybook/main.ts
@@ -0,0 +1,33 @@
+import { qwikVite } from '@builder.io/qwik/optimizer';
+import { rootMain } from '../../../.storybook/main';
+
+import type { StorybookViteConfig } from '@storybook/builder-vite';
+
+import { mergeConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+const config: StorybookViteConfig = {
+  ...rootMain,
+  core: { ...rootMain.core, builder: '@storybook/builder-vite' },
+  stories: [
+    ...rootMain.stories,
+    '../src/lib/**/*.stories.mdx',
+    '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [...(rootMain.addons || [])],
+  framework: {
+    name: 'storybook-framework-qwik',
+  },
+  viteFinal: async (config: any) => {
+    return mergeConfig(config, {
+      plugins: [
+        viteTsConfigPaths({
+          root: '../../../',
+        }),
+        qwikVite(),
+      ],
+    });
+  },
+};
+
+export default config;
