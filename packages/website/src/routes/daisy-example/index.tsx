2cdda2c3278c4bc4c7584e52dd0c115ca5b72a82
Glenn Becker <glenn@system76-pc.localdomain>
2023-01-15 16:49:11 -0700

    Add Daisy tooltip to the website

--- packages/website/src/routes/daisy-example/index.tsx
+++ packages/website/src/routes/daisy-example/index.tsx
@@ -1,6 +1,6 @@
 import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Collapse, Drawer, Tab, TabPanel, Tabs, Toggle } from '@qwik-ui/daisy';
+import { Collapse, Drawer, Tab, TabPanel, Tabs, Toggle, Tooltip } from '@qwik-ui/daisy';
 
 export default component$(() => {
   const tabs = ['Tab 1', 'Tab 2', 'Tab 3'];
@@ -9,6 +9,26 @@ export default component$(() => {
 
   return (
     <div>
+
+<div>
+        Hey I am a text and you can &nbsp;
+        <Tooltip
+          inline={true}
+          position="bottom"
+          content="Hi this is the message"
+        >
+          hover on me
+          <div q:slot="tooltip-content">Custom thing</div>
+        </Tooltip>
+      </div>
+
+      <hr />
+
+      <Tooltip content="Hi this is the message">
+        <div style="width: 100px; height: 100px; background-color: red;"></div>
+        <div q:slot="tooltip-content">Custom thing</div>
+      </Tooltip>
+
       <div style="width: 300px">
         <Collapse showArrow={true}>
           <label q:slot="label">Hi Glenn and Gil!</label>
@@ -25,20 +45,20 @@ export default component$(() => {
           {tabs.map((tab, index) => {
             return (
               <Tab
+                key={index}
                 onClick$={(clicked: number) => {
                   activeTab.value = clicked;
                 }}
                 isLifted={false}
                 isBordered={true}
-                isActive={index === activeTab.value}
               >
                 {tab}
               </Tab>
             );
           })}
-          {tabs.map((tab) => {
+          {tabs.map((tab, index) => {
             return (
-              <TabPanel>
+              <TabPanel key={index}>
                 <div>
                   {tab} {tab} {tab}
                 </div>
@@ -89,10 +109,15 @@ export default component$(() => {
                 collapse-title text-xl font-medium collapse-content max-h-fit tabs tabs-boxed 
                 tab tab-active tab-bordered tab-lifted form-control abel cursor-pointer toggle label-text
                 drawer
+                bg-black
+                text-white
+                rounded-lg
+                p-2
                 drawer-toggle
                 btn btn-primary drawer-button
                 drawer-side
                 drawer-overlay
+                tooltip
                 `}
       />
     </div>
