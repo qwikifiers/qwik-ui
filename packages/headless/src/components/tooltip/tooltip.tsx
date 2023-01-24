a30971809b810e370e940b29a9e5a417505afcaf
Glenn Becker <nnelgxorz@gmail.com>
2023-01-24 09:52:00 -0700

    Set position style attributes directly in update instead of using signals.

--- packages/headless/src/components/tooltip/tooltip.tsx
+++ packages/headless/src/components/tooltip/tooltip.tsx
@@ -8,7 +8,7 @@ import {
   useSignal,
   useStylesScoped$,
 } from '@builder.io/qwik';
-import { computePosition, type ComputePositionConfig } from '@floating-ui/dom';
+import { computePosition, flip, type ComputePositionConfig } from '@floating-ui/dom';
 import styles from './tooltip.css?inline';
 
 export interface TooltipProps {
@@ -22,29 +22,36 @@ export interface TooltipProps {
 type State = 'hidden' | 'positioned' | 'unpositioned' | 'closing';
 
 export const Tooltip = component$(
-  ({ content, position = 'top', durationMs = 100, ...props }: TooltipProps) => {
+  ({ content, position = 'top', durationMs = 0, ...props }: TooltipProps) => {
     useStylesScoped$(styles);
     const id = useId();
     const triggerAnchor = useSignal<HTMLElement>();
     const tooltipAnchor = useSignal<HTMLElement>();
     const stateSignal = useSignal<State>('hidden');
-    const positionSignal = useSignal<{ x: number; y: number }>({ x: 0, y: 0 });
 
     const Wrapper: keyof HTMLElementTagNameMap = props.inline ? 'span' : 'div';
 
     const update = $(async () => {
-      if (triggerAnchor.value && tooltipAnchor.value) {
-        const { x, y } = await computePosition(
-          triggerAnchor.value,
-          tooltipAnchor.value as HTMLElement,
+      const trigger = triggerAnchor.value;
+      const tooltip = tooltipAnchor.value;
+      if (trigger && tooltip) {
+        await computePosition(
+          trigger,
+          tooltip,
           {
             placement: position,
+            middleware: [flip()]
           }
-        );
-        positionSignal.value = { x, y };
+        ).then(({x, y}) => {
+          Object.assign(tooltip.style, {
+            left: `${x}px`,
+            top: `${y}px`
+          })
+        });
         stateSignal.value = 'positioned';
       }
     });
+
 
     const showTooltip = $(() => {
       stateSignal.value = 'unpositioned';
@@ -84,7 +91,6 @@ export const Tooltip = component$(
           onMouseLeave$={() => hideTooltip()}
           onFocus$={() => showTooltip()}
           onBlur$={() => hideTooltip()}
-          // need an id for the tooltip;
           aria-describedby={`#${id}`}
         >
           <Slot />
@@ -96,20 +102,15 @@ export const Tooltip = component$(
           onAnimationEnd$={() => {
             if (stateSignal.value == 'closing') {
               stateSignal.value = 'hidden';
-              positionSignal.value = { x: 0, y: 0 };
             }
           }}
           ref={tooltipAnchor}
           role="tooltip"
           {...props}
-          // Cannot be animated
-          style={`--duration: ${durationMs}ms;--x: ${
-            positionSignal.value.x || 0
-          }px; --y: ${positionSignal.value.y || 0}px;`}
+          style={`--duration: ${durationMs}ms;`}
           data-state={stateSignal.value}
         >
           {content}
-          {/* {content ? content : <Slot name="tooltip-content" />} */}
         </Wrapper>
       </>
     );
