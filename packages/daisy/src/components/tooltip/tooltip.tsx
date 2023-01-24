21a7a7ebdc8c4436beb5417957c9692f82c8cf1e
Glenn Becker <nnelgxorz@gmail.com>
2023-01-24 10:20:21 -0700

    Add 100ms animation duration to daisy tooltip

--- packages/daisy/src/components/tooltip/tooltip.tsx
+++ packages/daisy/src/components/tooltip/tooltip.tsx
@@ -9,6 +9,7 @@ export type TooltipProps = TooltipHeadlessProps;
 export const Tooltip = component$((props: TooltipProps) => {
   return (
     <TooltipHeadless
+      durationMs={100}
       {...props}
       class={
         'inline-block bg-black text-white p-2 rounded-lg z-[99] ' +
