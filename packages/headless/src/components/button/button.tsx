58f1ff1fcabbe9f0cfb66203000e84d76096d5b4
Fabio Biondi <fabiobiondi@users.noreply.github.com>
2023-01-28 01:02:12 +0100

    feat: add Button component (#98)
    * feat: add Button component
    
    * feat: add support for HTML Element props

---
+++ packages/headless/src/components/button/button.tsx
@@ -0,0 +1,10 @@
+import { component$, HTMLAttributes, Slot } from '@builder.io/qwik';
+
+type ButtonProps = {
+  disabled?: boolean;
+};
+
+export const Button = component$((props: ButtonProps & HTMLAttributes<HTMLButtonElement> ) => {
+  return <button {...props}><Slot /></button>;
+});
+
