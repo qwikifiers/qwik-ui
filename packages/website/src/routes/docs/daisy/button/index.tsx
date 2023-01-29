e6cda351edb9fdb3df8187d86c7ba73daeef08f9
Fabio Biondi <fabiobiondi@users.noreply.github.com>
2023-01-29 16:36:33 +0100

    fix: button dynamic classes (#106)
    * chore: added the shared folder
    
    * feat(component): now the Daisy Button component supports all the Daisy properties
    
    The button now supports all Daisy modifiers (primary, secondary), sizes (xs, sm, ...) and other
    properties. A new clsq utility  (a TS clone of the famous clsx utility) has been added a new shared
    folder (I have also created an alias in order to be easily imported everywhere)

--- packages/website/src/routes/docs/daisy/button/index.tsx
+++ packages/website/src/routes/docs/daisy/button/index.tsx
@@ -1,27 +1,90 @@
-import { component$, $ } from '@builder.io/qwik';
+import { component$, $, useStylesScoped$ } from '@builder.io/qwik';
 import { Button } from '@qwik-ui/theme-daisy';
 
 export default component$(() => {
+  useStylesScoped$(`
+    .panel {
+      display: flex;
+      gap: 8px;
+      flex-wrap: wrap;
+    }`
+  );
+
   return (
     <>
       <h2>This is the documentation for the Button</h2>
 
-      <div className="flex flex-col gap-8 mt-4">
-        <div>
-          <h2>Basic Example</h2>
-          <Button>SIMPLE BUTTON</Button>
+      <div class="flex flex-col gap-8 mt-4">
+        <h2>Basic Example</h2>
+        <div class="panel">
+          <Button>default</Button>
+          <Button primary>primary</Button>
+          <Button secondary>secondary</Button>
+          <Button accent>accent</Button>
+          <Button info>info</Button>
+          <Button success>success</Button>
+          <Button warning>warning</Button>
+          <Button error>error</Button>
+          <Button ghost>ghost</Button>
+          <Button link>link</Button>
+          <Button primary active>primary active</Button>
+          <Button accent active>accent active</Button>
+          <Button disabled>disabled</Button>
+          <Button disabled={true}>disabled</Button>
         </div>
 
-        <div>
-          <h2>Attributes</h2>
-          <Button type="button" disabled={true}>
-            DISABLED BUTTON
+        <h2>Outline Example</h2>
+        <div class="panel">
+          <Button outline primary>primary</Button>
+          <Button outline secondary>secondary</Button>
+          <Button outline accent>accent</Button>
+          <Button outline info>info</Button>
+          <Button outline success>success</Button>
+          <Button outline warning>warning</Button>
+          <Button outline error>error</Button>
+          <Button outline ghost>ghost</Button>
+          <Button outline link>link</Button>
+          <Button outline active>active</Button>
+          <Button outline disabled>disabled</Button>
+        </div>
+
+        <h2>size</h2>
+        <div class="panel">
+          <Button primary size="xs">xs</Button>
+          <Button primary size="sm">sm</Button>
+          <Button primary size="md">md</Button>
+          <Button primary size="lg">lg</Button>
+        </div>
+
+        <h2>loading</h2>
+        <div class="panel">
+          <Button primary loading>loading</Button>
+          <Button loading square></Button>
+        </div>
+
+        <h2>no click animation</h2>
+        <div class="panel">
+          <Button primary noAnimation>click me</Button>
+          <Button secondary noAnimation>click me</Button>
+        </div>
+
+        <h2>Square and Circle</h2>
+        <div class="panel">
+          <Button secondary square>A</Button>
+          <Button accent circle>B</Button>
+        </div>
+
+        <h2>Custom class</h2>
+
+        <div class="panel">
+          <Button type="button" primary class="px-32 border-4 border-black">
+            CUSTOM CLASS
           </Button>
         </div>
 
         <div>
-          <h2>Events</h2>
-          <Button onClick$={$(() => window.alert('hello'))}>SHOW ALERT</Button>
+          <h2>Qwik Events</h2>
+          <Button primary onClick$={$(() => window.alert('hello'))}>SHOW ALERT</Button>
         </div>
       </div>
     </>
