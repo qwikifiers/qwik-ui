4088b1e5fa0f93176f1b0ea4460b020111ddc0fb
gioboa <giorgiob.boa@gmail.com>
2023-01-30 16:27:06 +0100

    feat: add MaterialButtonProps

--- packages/material/src/components/button/button.tsx
+++ packages/material/src/components/button/button.tsx
@@ -1,14 +1,45 @@
 import { component$, QwikIntrinsicElements, Slot } from '@builder.io/qwik';
 import { Button as HeadlessButton } from '@qwik-ui/headless';
+import { clsq } from '@qwik-ui/shared';
 
-export type ButtonProps = QwikIntrinsicElements['button'];
+export type HTMLButtonProps = QwikIntrinsicElements['button'];
 
-export const Button = component$(
-  (props: ButtonProps) => {
-    return (
-      <HeadlessButton class="waves-effect waves-light btn-large" {...props}>
-        <Slot />
-      </HeadlessButton>
-    );
-  }
-);
+export type MaterialButtonProps = {
+  disabled?: boolean;
+  floating?: boolean;
+  flat?: boolean;
+  size?: 'small' | 'medium' | 'large';
+};
+
+export type ButtonProps = HTMLButtonProps & MaterialButtonProps;
+
+export const Button = component$((props: ButtonProps) => {
+  const {
+    class: cls,
+    disabled,
+    floating,
+    flat,
+    size = 'medium',
+    ...rest
+  } = props;
+
+  return (
+    <HeadlessButton
+      {...rest}
+      class={clsq(
+        {
+          disabled: disabled,
+          'btn-floating': floating,
+          'btn-flat': flat,
+          // size
+          'btn-small': size === 'small',
+          btn: size === 'medium',
+          'btn-large': size === 'large',
+        },
+        cls
+      )}
+    >
+      <Slot />
+    </HeadlessButton>
+  );
+});
--- packages/website/src/routes/docs/material/button/index.tsx
+++ packages/website/src/routes/docs/material/button/index.tsx
@@ -1,19 +1,46 @@
-import { component$, $ } from '@builder.io/qwik';
+import { component$, $, useStylesScoped$ } from '@builder.io/qwik';
 import { Button } from '@qwik-ui/material';
 import { MaterialContext } from '../../../../../src/components/material';
 
-
 export default component$(() => {
+  useStylesScoped$(`
+    .panel {
+      display: flex;
+      gap: 8px;
+      flex-wrap: wrap;
+    }`);
+
   return (
     <>
       <h2>This is the documentation for the Button</h2>
+
       <div class="flex flex-col gap-8 mt-4">
-        <div>
-          <h2>Basic Example</h2>
-          <MaterialContext>
-            <Button onClick$={$(() => alert('Material'))}>SIMPLE BUTTON</Button>
-          </MaterialContext>
-        </div>
+        <h2>Raised</h2>
+        <MaterialContext>
+          <Button>Raised</Button>
+        </MaterialContext>
+        <h2>Disabled</h2>
+        <MaterialContext>
+          <Button disabled>Disabled</Button>
+        </MaterialContext>
+        <h2>Floating</h2>
+        <MaterialContext>
+          <Button floating size='large'>
+            <i class="material-icons">add</i>
+          </Button>
+        </MaterialContext>
+        <h2>Flat</h2>
+        <MaterialContext>
+          <Button flat>Flat</Button>
+        </MaterialContext>
+        <h2>Size: default medium</h2>
+        <MaterialContext>
+          <div class="panel">
+            <Button size={'small'}>Small</Button>
+            <Button size={'medium'}>Medium</Button>
+            <Button size={'large'}>Large</Button>
+          </div>
+        </MaterialContext>
       </div>
     </>
   );
