e80cb3d1bf6fe17801d2b8b6d2840562ab5e20af
gioboa <giorgiob.boa@gmail.com>
2023-02-02 18:03:09 +0100

    fix: remove tailwind hack to prevent purge

--- packages/website/src/routes/docs/index.tsx
+++ packages/website/src/routes/docs/index.tsx
@@ -2,61 +2,7 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return (
-    <div>
-      Root Documentation Page
-      {/* hack to prevent tailwind purge */}
-      <div
-        style={{ display: 'none' }}
-        class={`collapse border border-base-300 rounded-box collapse-arrow collapse-plus
-                collapse-title text-xl font-medium collapse-content max-h-fit tabs tabs-boxed
-                tab tab-active tab-bordered tab-lifted form-control abel cursor-pointer toggle label-text
-                drawer
-                drawer-toggle
-                btn btn-primary drawer-button
-                drawer-side
-                drawer-overlay
-                btn
-                btn-primary
-                btn-secondary
-                btn-accent
-                btn-info
-                btn-success
-                btn-warning
-                btn-error
-                btn-ghost
-                btn-link
-                btn-outline
-                btn-active
-                btn-disabled
-                
-                glass
-                loading
-                no-animation
-                
-                btn-xs
-                btn-sm
-                btn-md
-                btn-lg
-                btn-wide
-                btn-block
-                btn-circle
-                btn-square
-                
-                card
-                card-title
-                card-body
-                card-actions
-                
-                flex
-                justify-start
-                justify-center
-                justify-between
-                justify-end
-                `}
-      />
-    </div>
-  );
+  return <div>Root Documentation Page</div>;
 });
 
 export const head: DocumentHead = {
