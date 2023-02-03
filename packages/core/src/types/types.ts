c20e791ae71f7adb37c075180b154f7fd6660d3c
gioboa <giorgiob.boa@gmail.com>
2023-02-03 08:36:35 +0100

    refactor: remove deprecated core
    the core package is still available in the deprecated-core branch

--- packages/core/src/types/types.ts
+++
@@ -1,7 +0,0 @@
-export type MessageTypes = 'success' | 'info' | 'warning' | 'error';
-export type ColorTypes = 'primary' | 'secondary' | 'accent' | MessageTypes;
-export type Positions = 'top' | 'bottom' | 'left' | 'right';
-export type VerticalOptions = 'top' | 'middle' | 'bottom';
-export type HorizontalOptions = 'start' | 'center' | 'end';
-export type Size = 'w-8' | 'w-16' | 'w-20' | 'w-24' | 'w-32';
-export type ResponsiveSize = 'lg' | 'md' | 'sm' | 'xs';
