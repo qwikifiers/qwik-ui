2796569a003387d2193569d5c5a501b21a4044f1
gioboa <giorgiob.boa@gmail.com>
2023-02-05 13:34:26 +0100

    feat: root page

--- packages/website/src/routes/index.tsx
+++ packages/website/src/routes/index.tsx
@@ -1,30 +1,18 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { Link } from '@builder.io/qwik-city';
 
 export default component$(() => {
   return (
-    <div>
-      <div>Root Documentation Page</div>
-      <>
-        <ul>
-          <li>
-            <Link href="/install">
-              <span class="subtitle">Install</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/use">
-              <span class="subtitle">Use</span>
-            </Link>
-          </li>
-          <li>
-            <Link href="/docs">
-              <span class="subtitle">Components documentation</span>
-            </Link>
-          </li>
-        </ul>
-      </>
+    <div class="flex flex-col items-center">
+      <h1 class="text-5xl text-black dark:text-white">Qwik UI</h1>
+      <p class="mt-6 text-lg text-black dark:text-white">
+        The components library for Qwik
+      </p>
+      <a href="/docs">
+        <button class="link text-black dark:text-white" type="button">
+          All components
+        </button>
+      </a>
     </div>
   );
 });
