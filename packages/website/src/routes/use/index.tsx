f353700f23c36da96638c11cd2a2ea0cacc5180a
amit <amit@contextive.com>
2023-02-08 16:45:57 +0200

    Nicer "install" and "usage" routes UI for docs website

--- packages/website/src/routes/use/index.tsx
+++ packages/website/src/routes/use/index.tsx
@@ -2,7 +2,14 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return <div>How to use page</div>;
+  return (
+    <div>
+      <h1 class="text-xl">Getting Started</h1>
+      <div class="mt-4">
+        <p>How to use page</p>
+      </div>
+    </div>
+  );
 });
 
 export const head: DocumentHead = {
