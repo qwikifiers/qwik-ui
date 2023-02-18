019f7fa1e72ef7bd85c19adec3184add33e55010
Itai Mizlish <37772742+itaim18@users.noreply.github.com>
2023-02-18 23:08:43 +0200

    feat(component): added a Toast component that reads the label, the po… (#157)
    * feat(component): added a Toast component that reads the label, the position and alert type
    
    * feat(fix last commit: toast comp.): changing by the review advices
    
    commit #157
    
    * fix ToastProps
    
    ---------
    
    Co-authored-by: Giorgio Boa <35845425+gioboa@users.noreply.github.com>

--- packages/headless/src/index.ts
+++ packages/headless/src/index.ts
@@ -7,6 +7,7 @@ export * from './components/menu/menu';
 export * from './components/popover';
 export * from './components/rating/rating';
 export * from './components/tabs/tabs';
+export * from './components/toast/toast';
 export * from './components/toggle/toggle';
 export * from './components/tooltip/tooltip';
 export * as Select from './components/select/select';
