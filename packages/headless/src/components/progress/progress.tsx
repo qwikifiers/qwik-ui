ce182b33aaa48d8663db4d3704cee4f5454613f7
Luis Beqja <luisbeqjamw@gmail.com>
2023-02-19 23:16:01 +0100

    feat(component): add new  Progress Bar component
    fix #129

---
+++ packages/headless/src/components/progress/progress.tsx
@@ -0,0 +1,11 @@
+import { component$, QwikIntrinsicElements, Slot } from '@builder.io/qwik';
+
+export type ProgressProps = QwikIntrinsicElements['progress'];
+
+export const Progress = component$((props: ProgressProps) => {
+  return (
+    <progress {...props}>
+      <Slot />
+    </progress>
+  );
+});
