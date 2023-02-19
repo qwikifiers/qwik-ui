627628e30681842bd1c16109108a71593e82b9a1
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2023-02-19 13:57:08 +0100

    fix: website issues (#170)
    * fix website
    
    * chore: remove website_old
    
    if we need it we can use git history

--- apps/website_old/src/components/material/material-provider/material-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import material from '../styles/materialize.scss?inline';
-import materialIcons from '../styles/material-icons.css?inline';
-
-export const MaterialProvider = component$(() => {
-  useStyles$(material);
-  useStyles$(materialIcons);
-  return <Slot />;
-});
--- apps/website_old/src/components/material/withMaterialContext/withMaterialContext.tsx
+++
@@ -1,9 +0,0 @@
-import { component$, Slot } from '@builder.io/qwik';
-
-export const WithMaterialStyles = component$(() => {
-  return (
-    <div class="qwik-ui-material-theme">
-      <Slot />
-    </div>
-  );
-});
