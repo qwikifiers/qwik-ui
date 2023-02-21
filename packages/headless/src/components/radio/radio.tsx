bce389345488d124151ab1e9df8f66ffcc7c8cf4
Luis Beqja <luisbeqjamw@gmail.com>
2023-02-21 23:55:15 +0100

    feat: inline radio headless component

--- packages/headless/src/components/radio/radio.tsx
+++ packages/headless/src/components/radio/radio.tsx
@@ -2,10 +2,6 @@ import { component$, QwikIntrinsicElements } from '@builder.io/qwik';
 
 export type RadioProps = QwikIntrinsicElements['input'];
 
-export const Radio = component$((props: RadioProps) => {
-  return (
-    <>
-      <input {...props} type="radio" />
-    </>
-  );
-});
+export const Radio = component$((props: RadioProps) => (
+  <input {...props} type="radio" />
+));
