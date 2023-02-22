ff6016f88f19a6a0f915afe5f70f5576e3c3be1a
Alessandro Sebastiani <sebbalex@users.noreply.github.com>
2023-02-22 18:10:32 +0100

    fix: deprecated useClientEffect$ and createContext hooks (#187)

--- packages/headless/src/components/popover/popover-content.tsx
+++ packages/headless/src/components/popover/popover-content.tsx
@@ -1,24 +1,26 @@
-import { component$, Slot, useClientEffect$, useContext, useSignal, useStylesScoped$ } from '@builder.io/qwik';
+import {
+  component$,
+  Slot,
+  useBrowserVisibleTask$,
+  useContext,
+  useSignal,
+  useStylesScoped$,
+} from '@builder.io/qwik';
 import styles from './popover-content.css?inline';
 import { PopoverContext } from './popover-context';
 
-export const PopoverContent = component$(
-  () => {
-    const ref = useSignal<HTMLElement>();
-    const contextService = useContext(PopoverContext);
-    useStylesScoped$(styles);
+export const PopoverContent = component$(() => {
+  const ref = useSignal<HTMLElement>();
+  const contextService = useContext(PopoverContext);
+  useStylesScoped$(styles);
 
-    useClientEffect$(() => {
-      contextService.setOverlayRef$(ref);
-    });
+  useBrowserVisibleTask$(() => {
+    contextService.setOverlayRef$(ref);
+  });
 
-    return (
-      <div
-        ref={ref}
-        class="popover-content">
-        <Slot />
-      </div>
-    );
-  }
-);
-
+  return (
+    <div ref={ref} class="popover-content">
+      <Slot />
+    </div>
+  );
+});
