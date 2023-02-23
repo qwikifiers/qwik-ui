da80bea131a4b8a9a5d7867be97114c1cee41e90
Leonardo Montini <lion.48m@gmail.com>
2023-02-23 22:25:55 +0100

    feat(slider): expose style and class to customize the slider component

--- packages/headless/src/components/slider/sliderProgress.tsx
+++ packages/headless/src/components/slider/sliderProgress.tsx
@@ -1,20 +1,21 @@
 import { component$, Slot, useContext } from '@builder.io/qwik';
 import { sliderContext } from './slider';
 
-export const SliderProgress = component$(() => {
+export const SliderProgress = component$(({ style }: { style?: string }) => {
   const contextService = useContext(sliderContext);
 
   return (
     <div
-      style={{
-        display: 'block',
-        position: 'absolute',
-        top: 0,
-        height: '100%',
-        left: 0,
-        width: `${contextService.percentage.value}%`,
-        background: 'rgb(0,117,255)',
-      }}
+      style={`
+        display: block;
+        position: absolute;
+        top: 0;
+        height: 100%;
+        left: 0;
+        width: ${contextService.percentage.value}%;
+        background: rgb(0,117,255);
+        ${style ?? ''}
+      `}
     >
       <Slot />
     </div>
