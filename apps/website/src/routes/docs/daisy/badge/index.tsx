6e834527354a7763f636685ce21b214cd46f96b3
riccardovettore <riccardo.vettore.dev@gmail.com>
2023-03-01 13:28:17 +0100

    feat(component): update badge component
    #219

--- apps/website/src/routes/docs/daisy/badge/index.tsx
+++ apps/website/src/routes/docs/daisy/badge/index.tsx
@@ -4,20 +4,18 @@ import { Badge, Button } from '@qwik-ui/theme-daisy';
 export default component$(() => {
   useStylesScoped$(`
     h1 { margin: 0.5rem 0 1rem 0; padding-top: 1rem; font-weight: bold; }
-    p { margin-bottom: 0.5rem; } .container { display: flex; gap: 5px}
   `);
   return (
     <>
-      <h2>This is the documentation for the Spinner</h2>
+      <h2>This is the documentation for the Badge</h2>
 
       <h1>Badge Example</h1>
 
-      <Badge />
+      <Badge>badge</Badge>
 
       <h1>Variant</h1>
-      <p>Set the variant attribute to change the badge's variant.</p>
 
-      <div class="container">
+      <div class="flex gap-2">
         <Badge>neutral</Badge>
         <Badge variant="primary">primary</Badge>
         <Badge variant="secondary">secondary</Badge>
@@ -30,7 +28,7 @@ export default component$(() => {
       </div>
 
       <h1>Outline</h1>
-      <div class="container">
+      <div class="flex gap-2">
         <Badge outline>neutral</Badge>
         <Badge variant="primary" outline>
           primary
@@ -59,21 +57,29 @@ export default component$(() => {
       </div>
 
       <h1>Badge sizes</h1>
-      <div class="container">
+      <div class="flex gap-2">
         <Badge size="lg">Badge lg</Badge>
         <Badge size="md">Badge md</Badge>
         <Badge size="sm">Badge sm</Badge>
         <Badge size="xs">Badge xs</Badge>
       </div>
 
+      <h1>Empty badge</h1>
+      <div class="flex gap-2">
+        <Badge size="lg" />
+        <Badge size="md" />
+        <Badge size="sm" />
+        <Badge size="xs" />
+      </div>
+
       <h1>Badge with state colors</h1>
-      <div class="container">
+      <div class="flex gap-2">
         <Badge variant="info" class="gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block w-4 h-4 stroke-current"
+            class="inline-block w-4 h-4 stroke-current"
           >
             <path
               stroke-linecap="round"
@@ -90,7 +96,7 @@ export default component$(() => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block w-4 h-4 stroke-current"
+            class="inline-block w-4 h-4 stroke-current"
           >
             <path
               stroke-linecap="round"
@@ -106,7 +112,7 @@ export default component$(() => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block w-4 h-4 stroke-current"
+            class="inline-block w-4 h-4 stroke-current"
           >
             <path
               stroke-linecap="round"
@@ -122,7 +128,7 @@ export default component$(() => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            className="inline-block w-4 h-4 stroke-current"
+            class="inline-block w-4 h-4 stroke-current"
           >
             <path
               stroke-linecap="round"
@@ -135,26 +141,28 @@ export default component$(() => {
         </Badge>
       </div>
 
-      <h1>Badge with state colors</h1>
-      <h2 class="text-xl">
-        Heading
-        <Badge size="lg">NEW</Badge>
-      </h2>
-      <h3 class="text-lg">
-        Heading
-        <Badge size="md">NEW</Badge>
-      </h3>
-      <h4 class="text-base">
-        Heading
-        <Badge size="sm">NEW</Badge>
-      </h4>
-      <h5 class="text-sm">
-        Heading
-        <Badge size="xs">NEW</Badge>
-      </h5>
+      <h1>Badge in a text</h1>
+      <div class="flex flex-col gap-2">
+        <h2 class="text-xl">
+          Heading
+          <Badge size="lg">NEW</Badge>
+        </h2>
+        <h3 class="text-lg">
+          Heading
+          <Badge size="md">NEW</Badge>
+        </h3>
+        <h4 class="text-base">
+          Heading
+          <Badge size="sm">NEW</Badge>
+        </h4>
+        <h5 class="text-sm">
+          Heading
+          <Badge size="xs">NEW</Badge>
+        </h5>
+      </div>
 
       <h1>Badge in a button</h1>
-      <div class="container">
+      <div class="flex gap-2">
         <Button variant="primary" class="gap-2">
           Inbox
           <Badge variant="primary">+4</Badge>
