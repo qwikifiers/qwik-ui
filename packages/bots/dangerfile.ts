11a6d8db4363e56d9f03a719a179447e63b4556f
gioboa <giorgiob.boa@gmail.com>
2023-03-05 16:11:55 +0100

    test: change dangerfile.ts

--- packages/bots/dangerfile.ts
+++ packages/bots/dangerfile.ts
@@ -1,7 +1,7 @@
-import { danger, fail, warn } from 'danger';
+import { danger, fail } from 'danger';
 
 const hasPackageChanges = danger.git.modified_files.includes('package.json');
 const hasLockfileChanges = danger.git.modified_files.includes('pnpm-lock.yaml');
 if (hasPackageChanges && !hasLockfileChanges) {
-  warn('There are package.json changes with no corresponding lockfile changes');
+  fail('There are package.json changes with no corresponding lockfile changes');
 }
