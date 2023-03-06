931eff8193c3055c8030cb04ccc432c4a6900ded
gioboa <giorgiob.boa@gmail.com>
2023-03-06 23:09:42 +0100

    fix: remove daisy from headless docs

--- apps/website/src/routes/docs/headless/checkbox/index.tsx
+++ apps/website/src/routes/docs/headless/checkbox/index.tsx
@@ -23,14 +23,14 @@ export default component$(() => {
           name="test"
           value="test"
           onChange={$(() => console.log('clicked'))}
-          class="checkbox-secondary"
+          class="checkbox-secondary checkbox-margin-right"
         />
         test
       </Checkbox.Label>
 
       <h1>With label</h1>
       <Checkbox.Label htmlFor="test">
-        <Checkbox.Root class="mr-2" />
+        <Checkbox.Root class="checkbox-margin-right" />
         test
       </Checkbox.Label>
 
@@ -46,36 +46,64 @@ export default component$(() => {
 
       <h1>Size</h1>
       <fieldset class="">
-        <Checkbox.Root disabled checked class="qui-checkbox-sm" />
-        <Checkbox.Root disabled checked class="qui-checkbox-md ml-3" />
-        <Checkbox.Root disabled checked class="qui-checkbox-lg ml-3" />
+        <Checkbox.Root
+          disabled
+          checked
+          class="qui-checkbox-sm checkbox-margin-right"
+        />
+        <Checkbox.Root
+          disabled
+          checked
+          class="qui-checkbox-md checkbox-margin-right"
+        />
+        <Checkbox.Root disabled checked class="qui-checkbox-lg" />
       </fieldset>
 
       <h1>With legend</h1>
       <fieldset>
         <legend>group</legend>
-        <Checkbox.Root disabled checked class="qui-checkbox-sm mr-2" />
-        <Checkbox.Root disabled checked class="qui-checkbox-md mr-2" />
-        <Checkbox.Root disabled checked class="qui-checkbox-lg mr-2" />
+        <Checkbox.Root
+          disabled
+          checked
+          class="qui-checkbox-sm checkbox-margin-right"
+        />
+        <Checkbox.Root
+          disabled
+          checked
+          class="qui-checkbox-md checkbox-margin-right"
+        />
+        <Checkbox.Root disabled checked class="qui-checkbox-lg" />
       </fieldset>
 
       <h1>With legend group</h1>
       <legend>group</legend>
       <fieldset>
         <Checkbox.Label htmlFor="test">
-          <Checkbox.Root disabled checked class="qui-checkbox-sm mr-2" />
+          <Checkbox.Root
+            disabled
+            checked
+            class="qui-checkbox-sm checkbox-margin-right"
+          />
           test
         </Checkbox.Label>
       </fieldset>
       <fieldset>
         <Checkbox.Label htmlFor="test">
-          <Checkbox.Root disabled checked class="qui-checkbox-sm mr-2" />
+          <Checkbox.Root
+            disabled
+            checked
+            class="qui-checkbox-sm checkbox-margin-right"
+          />
           test
         </Checkbox.Label>
       </fieldset>
       <fieldset>
         <Checkbox.Label htmlFor="test">
-          <Checkbox.Root disabled checked class="qui-checkbox-sm mr-2" />
+          <Checkbox.Root
+            disabled
+            checked
+            class="qui-checkbox-sm checkbox-margin-right"
+          />
           test
         </Checkbox.Label>
       </fieldset>
