78e75e95da99b7335c9342de0ded300d11d85ee3
gioboa <giorgiob.boa@gmail.com>
2023-03-07 23:20:41 +0100

    feat: improve PR linter

--- packages/bots/dangerfile.ts
+++ packages/bots/dangerfile.ts
@@ -1,7 +1,27 @@
-import { danger, fail } from 'danger';
+import { danger, fail, warn } from 'danger';
 
-const hasPackageChanges = danger.git.modified_files.includes('package.json');
-const hasLockfileChanges = danger.git.modified_files.includes('pnpm-lock.yaml');
-if (hasPackageChanges && !hasLockfileChanges) {
-  fail('There are package.json changes with no corresponding lockfile changes');
-}
+const warnBigPR = () => {
+  const changesPRThreshold = 300;
+  const filesPRThreshold = 300;
+  const newFiles = danger.github.pr.changed_files;
+  const changes = danger.github.pr.additions + danger.github.pr.deletions;
+  if (changes > changesPRThreshold || newFiles > filesPRThreshold) {
+    warn(
+      `:exclamation: Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR for faster, easier review.`
+    );
+  }
+};
+
+const blockMismatches = () => {
+  const hasPackageChanges = danger.git.modified_files.includes('package.json');
+  const hasLockfileChanges =
+    danger.git.modified_files.includes('pnpm-lock.yaml');
+  if (hasPackageChanges && !hasLockfileChanges) {
+    fail(
+      'There are package.json changes with no corresponding lockfile changes'
+    );
+  }
+};
+
+warnBigPR();
+blockMismatches();
