c54224ffed04f2981732022f064cfed9c217bcbe
gioboa <giorgiob.boa@gmail.com>
2023-03-07 23:35:59 +0100

    test: test linter

--- packages/daisy copy/src/components/popover/popover.tsx
+++
@@ -1,7 +0,0 @@
-import { component$, QwikIntrinsicElements } from '@builder.io/qwik';
-
-export type PopoverProps = QwikIntrinsicElements['div'];
-
-export const Popover = component$((props: PopoverProps) => {
-  return <div {...props}>TODO</div>;
-});
