4dd2c0c17bffe91d70256630e33c1754e6b13712
GrandSchtroumpf <francois.guezengar@hotmail.fr>
2023-03-13 15:39:40 +0100

    feat(toast) Use html output element

--- packages/primitives/src/lib/toast/toast.tsx
+++ packages/primitives/src/lib/toast/toast.tsx
@@ -4,12 +4,18 @@ export type ToastProps = {
    */
   label?: string;
   class?: string;
+  /**
+   * Aria role: default 'status'.
+   * @description The toast uses the html element output, which has implicit role 'status'
+   * For information that requires an immediate attention of the user use 'alert'.
+   */
+  role?: 'alert' | 'status';
 }
 
 export const Toast = ({ label = 'New Message', ...toastProps }: ToastProps) => {
   return (
-    <div {...toastProps}>
+    <output {...toastProps}>
       <span>{label}</span>
-    </div>
+    </output>
   );
 };
