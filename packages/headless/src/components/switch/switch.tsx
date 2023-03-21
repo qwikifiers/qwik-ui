05709e2273ec24418099dee675380c599fd699f4
Thomas LEPEROU <thomas.leperou@gmail.com>
2023-03-21 17:27:22 +1100

    feat: clean up dom and styles

--- packages/headless/src/components/switch/switch.tsx
+++ packages/headless/src/components/switch/switch.tsx
@@ -10,11 +10,5 @@ export type SwitchProps = QwikIntrinsicElements['input'];
 export const Switch = component$((props: SwitchProps) => {
   useStylesScoped$(styles);
 
-  return (
-    <>
-      <div class="switch">
-        <input {...props} type="checkbox" role="switch" />
-      </div>
-    </>
-  );
+  return <input {...props} type="checkbox" role="switch" />;
 });
