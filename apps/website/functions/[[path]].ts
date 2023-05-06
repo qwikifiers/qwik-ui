94eb57035992f854e55ebf79bf1ba8de8bbe2509
Shai Reznik <shairez@users.noreply.github.com>
2023-05-06 20:58:15 +0300

    chore: updated cloudflare scripts

--- apps/website/functions/[[path]].ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line
-// @ts-ignore
-
-// Cloudflare Pages Functions
-// https://developers.cloudflare.com/pages/platform/functions/
-export { onRequest } from '../../../dist/apps/website/server/entry.cloudflare-pages';
