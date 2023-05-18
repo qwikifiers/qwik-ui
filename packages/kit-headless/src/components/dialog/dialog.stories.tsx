c846303348f48912fa9cd939f05f8a02320c0a3b
Gregor Woiwode <gregor.woiwode@gmail.com>
2023-05-18 21:34:37 +0200

    test(dialog): add storybook story

---
+++ packages/kit-headless/src/components/dialog/dialog.stories.tsx
@@ -0,0 +1,48 @@
+import { expect } from '@storybook/jest';
+import { userEvent, within } from '@storybook/testing-library';
+import { Meta, StoryObj } from 'storybook-framework-qwik';
+import * as Dialog from './public_api';
+
+const meta: Meta = {
+  title: 'Dialog',
+  component: Dialog.Root,
+};
+
+type Story = StoryObj;
+
+export default meta;
+
+export const Primary: Story = {
+  args: {
+    dialogTrigger: {
+      text: 'Open Dialog',
+    },
+    dialogPortal: {
+      text: 'Hello World',
+    },
+    dialogClose: {
+      text: 'Close',
+    },
+  },
+  render: (args) => (
+    <>
+      <Dialog.Root>
+        <Dialog.Trigger>
+          <button>{args.dialogTrigger.text}</button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          {args.dialogPortal.text}
+          <Dialog.Close>
+            <button>{args.dialogClose.text}</button>
+          </Dialog.Close>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </>
+  ),
+  play: ({ canvasElement, args }) => {
+    const canvas = within(canvasElement);
+    userEvent.click(canvas.getByText(args.dialogTrigger.text));
+    expect(canvas.getByText(args.dialogPortal.text)).toBeTruthy();
+    userEvent.click(canvas.getByText(args.dialogClose.text));
+  },
+};
