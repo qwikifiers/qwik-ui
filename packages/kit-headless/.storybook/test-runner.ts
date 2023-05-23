3b215a090df0ad36f3653dadf1785cac303ec7d3
Shai Reznik <shairez@users.noreply.github.com>
2023-05-23 22:24:43 +0300

    chore: attempt to run storybook with axe

---
+++ packages/kit-headless/.storybook/test-runner.ts
@@ -0,0 +1,23 @@
+import { injectAxe, checkA11y } from 'axe-playwright';
+
+import type { TestRunnerConfig } from '@storybook/test-runner';
+
+/*
+ * See https://storybook.js.org/docs/react/writing-tests/test-runner#test-hook-api-experimental
+ * to learn more about the test-runner hooks API.
+ */
+const a11yConfig: TestRunnerConfig = {
+  async preRender(page) {
+    await injectAxe(page);
+  },
+  async postRender(page) {
+    await checkA11y(page, '#storybook-root', {
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true,
+      },
+    });
+  },
+};
+
+export default a11yConfig;
