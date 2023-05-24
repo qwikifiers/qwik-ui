983b1623d75a4cc998d643172faafec6d24f4600
Shai Reznik <shairez@users.noreply.github.com>
2023-05-24 03:39:43 +0300

    test: simplified a11y testing

--- packages/kit-headless/src/components/accordion/accordion.spec.tsx
+++ packages/kit-headless/src/components/accordion/accordion.spec.tsx
@@ -3,7 +3,6 @@ import { Accordion, AccordionItem } from './accordion';
 
 describe('Accordion', () => {
   it('should render an Accordion', () => {
-    cy.injectAxe({ axeCorePath: '../../node_modules/axe-core/axe.min.js' });
     mount(
       <Accordion class="accordion">
         <AccordionItem label="Heading 1">
@@ -25,6 +24,6 @@ describe('Accordion', () => {
     cy.get('.content').should('exist');
     cy.contains(/Heading 2/i).should('exist');
 
-    cy.checkA11y('[data-cy-root]');
+    cy.checkA11yForComponent();
   });
 });
