eecf9a51cb13be45eb2a4837780be26c65d2db6b
Jack Shelton <me@jackshelton.com>
2023-06-30 22:23:36 -0500

    refactor(autocomplete root): naming convention changes / refactor

--- packages/kit-headless/src/components/autocomplete/autocomplete-root.tsx
+++ packages/kit-headless/src/components/autocomplete/autocomplete-root.tsx
@@ -130,6 +130,7 @@ export const AutocompleteRoot = component$(
     const isExpanded = useSignal(false);
     const triggerRef = useSignal<HTMLElement>();
     const listBoxRef = useSignal<HTMLElement>();
+    const rootRef = useSignal<HTMLElement>();
     const labelRef = useSignal<HTMLElement>();
     const inputValue = useSignal(defaultValue ? defaultValue : '');
     const listBoxId = useId();
@@ -137,13 +138,11 @@ export const AutocompleteRoot = component$(
     const buttonId = useId();
     const activeOptionId = useSignal(null);
     const focusInput$ = $((inputId: string) => {
-      triggerRef.value
+      rootRef.value
         ?.querySelector<HTMLElement>(
           `[data-autocomplete-input-id="${inputId}"]`
         )
         ?.focus();
-
-      console.log(triggerRef.value);
     });
 
     const contextService: AutocompleteContext = {
@@ -228,6 +227,7 @@ export const AutocompleteRoot = component$(
           props.onKeyDown$,
         ]}
         {...props}
+        ref={rootRef}
       >
         <Slot />
       </div>
