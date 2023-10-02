f0a9024f9a2e69647ee78ab0554c8016ac72e8cf
Shai Reznik <shairez@users.noreply.github.com>
2023-10-02 18:28:27 +0300

    WIP tcva

---
+++ packages/cva/src/tcva.ts
@@ -0,0 +1,9 @@
+// import { cva } from './cva';
+// import { stringifyClassList } from './stringify-classlist';
+
+// export function tcva<Props>(classList: ClassList, config: Config<Props>) {
+//   const variantsFn = cva(classList, config);
+//   return function wrappedVariantsFn(props: Props): string {
+//     return twMerge(stringifyClassList(variantsFn(props)));
+//   };
+// }
