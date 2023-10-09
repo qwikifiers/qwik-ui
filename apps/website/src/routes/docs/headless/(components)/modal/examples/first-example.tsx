d969c3db5ded0bbc73f99209c2555928b3b1c375
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2023-10-09 12:59:08 +0200

    docs(modal): simplify example

--- apps/website/src/routes/docs/headless/(components)/modal/examples/first-example.tsx
+++ apps/website/src/routes/docs/headless/(components)/modal/examples/first-example.tsx
@@ -12,36 +12,34 @@ export default component$(() => {
   const showSignal = useSignal(false);
 
   return (
-    <>
-      <Modal bind:show={showSignal}>
-        <ModalTrigger>
-          <button class="p-1 focus:shadow-[0_0_0_2px] focus:shadow-[var(--qwik-light-blue)] focus:outline-none">
-            Open Modal
+    <Modal bind:show={showSignal}>
+      <ModalTrigger>
+        <button class="p-1 focus:shadow-[0_0_0_2px] focus:shadow-[var(--qwik-light-blue)] focus:outline-none">
+          Open Modal
+        </button>
+      </ModalTrigger>
+      <ModalPortal class="rounded-md p-4 shadow-md">
+        <ModalHeader>
+          <h2 class="text:md font-bold">Are your sure?</h2>
+        </ModalHeader>
+        <ModalContent class="pb-4 pt-4">
+          <p>Yes, I want to remove my account.</p>
+        </ModalContent>
+        <ModalFooter class="flex justify-end gap-4">
+          <button
+            class="rounded-sm border border-[var(--qwik-light-blue)] p-2 text-[var(--qwik-light-blue)]"
+            onClick$={() => (showSignal.value = false)}
+          >
+            No
           </button>
-        </ModalTrigger>
-        <ModalPortal class="rounded-md p-4 shadow-md">
-          <ModalHeader>
-            <h2 class="text:md font-bold">Are your sure?</h2>
-          </ModalHeader>
-          <ModalContent class="pb-4 pt-4">
-            <p>Yes, I want to remove my account.</p>
-          </ModalContent>
-          <ModalFooter class="flex justify-end gap-4">
-            <button
-              class="rounded-sm border border-[var(--qwik-light-blue)] p-2 text-[var(--qwik-light-blue)]"
-              onClick$={() => (showSignal.value = false)}
-            >
-              No
-            </button>
-            <button
-              class="rounded-sm bg-[var(--qwik-light-blue)] p-2"
-              onClick$={() => (showSignal.value = false)}
-            >
-              Yes
-            </button>
-          </ModalFooter>
-        </ModalPortal>
-      </Modal>
-    </>
+          <button
+            class="rounded-sm bg-[var(--qwik-light-blue)] p-2"
+            onClick$={() => (showSignal.value = false)}
+          >
+            Yes
+          </button>
+        </ModalFooter>
+      </ModalPortal>
+    </Modal>
   );
 });
