3f10ed3a8540a8c27a5a6c6a5b235de10d6d6891
thejackshelton <me@jackshelton.com>
2023-10-10 22:58:29 -0500

    refactor(modal): performance refactor, moved useTasks into one

--- packages/kit-headless/src/components/modal/modal.tsx
+++ packages/kit-headless/src/components/modal/modal.tsx
@@ -22,7 +22,7 @@ export type ModalProps = Omit<QwikIntrinsicElements['dialog'], 'open'> & {
 };
 
 export const Modal = component$((props: ModalProps) => {
-  const refSig = useSignal<HTMLDialogElement>();
+  const modalRef = useSignal<HTMLDialogElement>();
 
   const { 'bind:show': givenOpenSig, show: givenShow, onShow$, onHide$ } = props;
   const defaultOpenSig = useSignal(false);
@@ -34,19 +34,29 @@ export const Modal = component$((props: ModalProps) => {
     showSig.value = openPropValue || false;
   });
 
-  useTask$(async function toggleModal({ track }) {
+  useTask$(async function toggleModal({ track, cleanup }) {
     const isOpen = track(() => showSig.value);
-    const modal = refSig.value;
+    const modal = modalRef.value;
 
     if (!modal) return;
 
+    let focusTrap: FocusTrap | null = createFocusTrap(modal, {
+      escapeDeactivates: false,
+    });
+
     if (isOpen) {
+      // open modal
       modal.showModal();
       await props.onShow$?.();
 
-      let scrollbarWidth: number | null = null;
+      // focus lock
+      focusTrap.activate();
+
+      // scroll lock
+      window.document.body.style.overflow = 'hidden';
 
       // prevents scrollbar flickers
+      let scrollbarWidth: number | null = null;
       if (scrollbarWidth === null) {
         scrollbarWidth = window.innerWidth - document.body.offsetWidth;
         document.body.style.overflow = 'hidden';
@@ -56,45 +66,18 @@ export const Modal = component$((props: ModalProps) => {
       await props.onHide$?.();
       modal.close();
     }
-  });
-
-  useTask$(function toggleFocusLock({ track, cleanup }) {
-    const isOpen = track(() => showSig.value);
-    const modal = refSig.value;
-
-    if (!modal) return;
-
-    let focusTrap: FocusTrap | null = createFocusTrap(modal, {
-      escapeDeactivates: false,
-    });
-
-    if (isOpen) {
-      focusTrap.activate();
-    }
 
     cleanup(() => {
       focusTrap?.deactivate();
       focusTrap = null;
-    });
-  });
 
-  useTask$(function toggleScrollLock({ track, cleanup }) {
-    const isOpen = track(() => showSig.value);
-    const modal = refSig.value;
-
-    if (!modal) return;
-
-    if (isOpen) {
-      window.document.body.style.overflow = 'hidden';
-    }
-
-    cleanup(() => {
+      // back to scroll
       window.document.body.style.overflow = '';
     });
   });
 
   const closeOnBackdropClick$ = $((event: QwikMouseEvent) => {
-    const modal = refSig.value;
+    const modal = modalRef.value;
 
     if (!modal) {
       return;
@@ -123,7 +106,7 @@ export const Modal = component$((props: ModalProps) => {
   return (
     <dialog
       {...props}
-      ref={refSig}
+      ref={modalRef}
       onClick$={(event) => closeOnBackdropClick$(event)}
       onClose$={() => (showSig.value = false)}
     >
