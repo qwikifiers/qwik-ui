1b6e9a57d48e3a8254064eb0d2ea8d18cfe41ed1
Fabio Biondi <info@fabiobiondi.com>
2023-11-05 23:22:14 +0100

    refactor(pagination): implement new version

--- apps/website/src/routes/docs/headless/(components)/pagination/examples/basic.tsx
+++ apps/website/src/routes/docs/headless/(components)/pagination/examples/basic.tsx
@@ -1,8 +1,201 @@
-import { component$, useSignal, type QwikIntrinsicElements } from '@builder.io/qwik';
-import { Modal, ModalContent, ModalFooter, ModalHeader } from '@qwik-ui/headless';
+import { component$, useSignal, $ } from '@builder.io/qwik';
+import { Pagination } from '@qwik-ui/headless';
+import { Toggle } from '@qwik-ui/tailwind';
+import { Simulate } from 'react-dom/test-utils';
+import select = Simulate.select;
 
 export default component$(() => {
-  const showSig = useSignal(false);
+  const selectedPage = useSignal(1);
+  const totalPages = useSignal(10);
 
-  return <>ciao giorgio</>;
+  const showFirstButton = useSignal(true);
+  const showLastButton = useSignal(true);
+  const hideNextButton = useSignal(true);
+  const hidePrevButton = useSignal(true);
+  const siblingCount = useSignal(1);
+  const boundaryCount = useSignal(1);
+
+  return (
+    <div class="mt-4 flex flex-col gap-6">
+      <h2>This is the documentation for the Pagination</h2>
+
+      <div
+        class="flex flex-col items-stretch gap-2"
+        style={{
+          width: '250px',
+        }}
+      >
+        <Toggle
+          pressed={showFirstButton.value}
+          onClick$={() => {
+            showFirstButton.value = !showFirstButton.value;
+          }}
+          label="showFirstButton"
+        />
+
+        <Toggle
+          pressed={showLastButton.value}
+          onClick$={() => {
+            showLastButton.value = !showLastButton.value;
+          }}
+          label="showLastButton"
+        />
+
+        <Toggle
+          pressed={hideNextButton.value}
+          onClick$={() => {
+            hideNextButton.value = !hideNextButton.value;
+          }}
+          label="hideNextButton"
+        />
+
+        <Toggle
+          pressed={hidePrevButton.value}
+          onClick$={() => {
+            hidePrevButton.value = !hidePrevButton.value;
+          }}
+          label="hidePrevButton"
+        />
+
+        <label class="flex items-center justify-between gap-10">
+          pages
+          <input
+            type="number"
+            style={{
+              width: '50px',
+              background: 'transparent',
+              textAlign: 'right',
+            }}
+            value={totalPages.value}
+            onChange$={(e) => {
+              totalPages.value = Number(e.target.value);
+            }}
+          />
+        </label>
+
+        <label class="flex items-center justify-between gap-10">
+          siblingCount
+          <input
+            type="number"
+            style={{
+              width: '50px',
+              background: 'transparent',
+              textAlign: 'right',
+            }}
+            value={siblingCount.value}
+            onChange$={(e) => {
+              siblingCount.value = Number(e.target.value);
+            }}
+          />
+        </label>
+
+        <label class="flex items-center justify-between gap-10">
+          boundaryCount
+          <input
+            type="number"
+            style={{
+              width: '50px',
+              background: 'transparent',
+              textAlign: 'right',
+            }}
+            value={boundaryCount.value}
+            onChange$={(e) => {
+              boundaryCount.value = Number(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+
+      <Pagination
+        selectedPage={selectedPage.value}
+        totalPages={totalPages.value}
+        onPageChange$={(page) => {
+          selectedPage.value = page;
+        }}
+        selectedClass="bg-red-500"
+        gap={'10px'}
+      />
+      {/*
+
+      <Pagination
+        activeClass="bg-red-500"
+        pages={20} page={10} onPaging$={() => console.log('pippo')}/>
+
+          <h2>Interactive Demo</h2>
+
+        <Pagination
+          pages={pages.value}
+          page={page.value}
+          onPaging$={(newValue: number) => {
+            page.value = newValue;
+          }}
+          showFirstButton={showFirstButton.value}
+          showLastButton={showLastButton.value}
+          hideNextButton={hideNextButton.value}
+          hidePrevButton={hidePrevButton.value}
+          siblingCount={siblingCount.value}
+          boundaryCount={boundaryCount.value}
+        />*/}
+      {/*
+
+      <h2>Size</h2>
+      <div class="flex flex-col gap-8">
+        <Pagination pages={5} page={1} size="xs" />
+        <Pagination pages={5} page={1} size="sm" />
+        <Pagination pages={5} page={1} size="md" />
+        <Pagination pages={5} page={1} size="lg" />
+      </div>
+
+      <h2>Variants</h2>
+      <div class="flex flex-col gap-8">
+        <Pagination pages={5} page={1} />
+        <Pagination pages={5} page={1} variant="primary" />
+        <Pagination pages={5} page={1} variant="secondary" />
+        <Pagination pages={5} page={1} variant="accent" />
+        <Pagination pages={5} page={1} variant="disabled" />
+        <Pagination pages={5} page={1} variant="success" />
+        <Pagination pages={5} page={1} variant="error" />
+        <Pagination pages={5} page={1} variant="info" />
+        <Pagination pages={5} page={1} variant="link" />
+        <Pagination pages={5} page={1} variant="warning" />
+      </div>
+
+      <h2>Outline</h2>
+      <div class="flex flex-col gap-8">
+        <Pagination pages={5} page={1} outline />
+      </div>
+
+      <h2>Square</h2>
+      <div class="flex flex-col gap-8">
+        <Pagination pages={5} page={1} square />
+      </div>
+
+      <h2>Disabled</h2>
+      <div class="flex flex-col gap-8">
+        <Pagination pages={5} page={1} disabled={true} />
+      </div>
+
+      <h2>Custom styles and labels</h2>
+
+      <div class="flex flex-col gap-8">
+        <Pagination
+          pages={pages.value}
+          page={page.value}
+          onPaging$={(newValue: number) => {
+            page.value = newValue;
+          }}
+          showFirstButton={showFirstButton.value}
+          showLastButton={showLastButton.value}
+          hideNextButton={hideNextButton.value}
+          hidePrevButton={hidePrevButton.value}
+          siblingCount={siblingCount.value}
+          boundaryCount={boundaryCount.value}
+          activeClass="!bg-cyan-800"
+          defaultClass="bg-cyan-500"
+          labels={{ prev: '⬅️', next: '➡️', first: 'START', last: 'END' }}
+        />
+      </div>
+      */}
+    </div>
+  );
 });
--- packages/kit-headless/src/components/pagination/pagination.tsx
+++ packages/kit-headless/src/components/pagination/pagination.tsx
@@ -1,231 +1,72 @@
-import { $, Component, PropFunction, component$ } from '@builder.io/qwik';
+import type { PropFunction } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 
-/**
- * PAGINATION TODOs
- * - Get storybook testing to work
- *
- * PROPS
- * pageIndex: default pagination value
- * V activeStyles: The styles of the active page button
- * V normalStyles: The styles of the inactive page buttons
- * V customArrowTexts: { previous, next }
- * disabled: enable/disable paginator
- * color: primary | secondary
- * variant: outlined (show border without bg)
- * shape: rounded | square
- * size: 'sm' | 'md' | 'lg'
- * paginationRange (see https://mui.com/material-ui/react-pagination/#pagination-ranges)
- *
- */
-export interface PaginationProps extends PaginationOptions {
-  pages: number;
-  page: number;
-  onPaging$: PropFunction<(index: number) => void>;
-  RenderItem?: Component<PaginationButtonProps>;
-  RenderDivider?: Component<object>;
+export interface PaginationProps {
+  class?: string;
+  selectedPage: number;
+  totalPages: number;
+  onPageChange$: PropFunction<(page: number) => void>;
+
+  // styles
+  selectedClass?: string;
+  gap: number | string;
 }
 
-export interface PaginationOptions {
-  boundaryCount?: number;
-  siblingCount?: number;
-  hidePrevButton?: boolean;
-  hideNextButton?: boolean;
-  showFirstButton?: boolean;
-  showLastButton?: boolean;
-  activeClass?: string;
-  defaultClass?: string;
-  labels?: PaginationButtonLabels;
-}
-
-export interface PaginationButtonProps {
-  onClick$: PropFunction<() => void>;
-  disabled?: boolean;
-  'aria-label': string;
-  'aria-current'?: boolean;
-  key?: string | number;
-  activeClass?: string;
-  defaultClass?: string;
-  value: PaginationButtonValue;
-  labels?: PaginationButtonLabels;
-}
-
-export type PaginationButtonValue =
-  | 'prev'
-  | 'next'
-  | 'first'
-  | 'last'
-  // | 'divider'
-  // | 'start-ellipsis'
-  // | 'end-ellipsis'
-  | number
-  | string;
-
-export interface PaginationButtonLabels {
-  first?: string;
-  last?: string;
-  next?: string;
-  prev?: string;
-}
-
-const range = (start: number, end: number) => {
-  const length = end - start + 1;
-  return Array.from({ length }, (_, i) => start + i);
-};
-
-export const getPaginationButtons = (
-  page: number,
-  count: number,
-  labels: PaginationButtonLabels | undefined,
-  {
-    boundaryCount = 1,
-    siblingCount = 1,
-    hidePrevButton,
-    hideNextButton,
-    showFirstButton,
-    showLastButton,
-  }: PaginationOptions,
-): PaginationButtonValue[] => {
-  const startPages = range(1, Math.min(boundaryCount, count));
-  const endPages = range(Math.max(count - boundaryCount + 1, boundaryCount + 1), count);
-
-  const siblingsStart = Math.max(
-    Math.min(
-      page - siblingCount, // Natural start
-      count - boundaryCount - siblingCount * 2 - 1, // Lower boundary when page is high
-    ),
-    boundaryCount + 2, // Greater than startPages
-  );
-
-  const siblingsEnd = Math.min(
-    Math.max(
-      page + siblingCount, // Natural end
-      boundaryCount + siblingCount * 2 + 2, // Upper boundary when page is low
-    ),
-    endPages.length > 0 ? endPages[0] - 2 : count - 1, // Less than endPages
-  );
-
-  const items = [
-    ...(showFirstButton ? ['first'] : []),
-    ...(hidePrevButton ? [] : ['prev']),
-    ...startPages,
-
-    ...(siblingsStart > boundaryCount + 2
-      ? ['divider']
-      : boundaryCount + 1 < count - boundaryCount
-      ? [boundaryCount + 1]
-      : []),
-
-    ...range(siblingsStart, siblingsEnd),
-
-    ...(siblingsEnd < count - boundaryCount - 1
-      ? ['divider']
-      : count - boundaryCount > boundaryCount
-      ? [count - boundaryCount]
-      : []),
-
-    ...endPages,
-    ...(hideNextButton ? [] : ['next']),
-    ...(showLastButton ? ['last'] : []),
-  ];
-
-  return items;
-};
-
-export const PaginationButton = component$(
-  ({
-    'aria-label': ariaLabel,
-    disabled,
-    onClick$,
-    key,
-    value,
-  }: PaginationButtonProps) => {
+export const Pagination = component$<PaginationProps>(
+  ({ class: _class, selectedPage, totalPages, onPageChange$, gap, selectedClass }) => {
     return (
-      <button onClick$={onClick$} aria-label={ariaLabel} disabled={disabled} key={key}>
-        {value}
-      </button>
-    );
-  },
-);
-
-export const PaginationDivider = component$(() => {
-  return <span>...</span>;
-});
-
-/**
- * Pagination
- * ----------
- * A pagination component
- * first page is 1
- *
- * @example
- * <Pagination pages={15} page={store.page} onPaging$={incrementCount} />
- */
-export const Pagination = component$(
-  ({
-    RenderItem = PaginationButton,
-    RenderDivider = PaginationDivider,
-    onPaging$,
-    page,
-    pages,
-    activeClass,
-    defaultClass,
-    labels,
-    ...rest
-  }: PaginationProps) => {
-    const _onPaging$ = $((page: number) => {
-      if (page < 1 || page > pages) return;
-      onPaging$(page);
-    });
-
-    const itemClickHandler = $((item: PaginationButtonValue) =>
-      _onPaging$(
-        (() => {
-          switch (item) {
-            case 'first':
-              return 1;
-            case 'prev':
-              return page - 1;
-            case 'next':
-              return page + 1;
-            case 'last':
-              return pages;
-            default:
-              if (typeof item === 'number') return item;
-              return page;
-          }
-        })(),
-      ),
-    );
-
-    const items = getPaginationButtons(page, pages, labels, rest);
-
-    return (
-      <>
-        {items.map((item, i) => {
-          return (
-            <>
-              {item === 'divider' ? (
-                <RenderDivider key={i} />
-              ) : (
-                <RenderItem
-                  activeClass={activeClass}
-                  defaultClass={defaultClass}
-                  key={i}
-                  labels={labels}
-                  onClick$={() => itemClickHandler(item)}
-                  disabled={
-                    (['prev', 'first'].includes(item.toString()) && page === 1) ||
-                    (['next', 'last'].includes(item.toString()) && page === pages)
-                  }
-                  aria-label={`Page ${item}`}
-                  aria-current={item === page}
-                  value={item}
-                />
-              )}
-            </>
-          );
-        })}
-      </>
+      <nav role="navigation" aria-label="pagination" data-testid="pagination">
+        <button
+          type="button"
+          aria-label={'prevAriaLabel'}
+          disabled={selectedPage <= 1}
+          onClick$={() => {
+            if (selectedPage !== 1) {
+              onPageChange$(selectedPage - 1);
+            }
+          }}
+        >
+          <div q:slot="prefix">⬅️</div>
+          <span>PREV</span>
+        </button>
+        <ul style={{ display: 'flex', gap: gap }}>
+          {[...new Array(totalPages + 1).keys()].map((page: number) => {
+            if (page === 0) return null;
+            return (
+              <li key={page}>
+                <div>
+                  <button
+                    class={{
+                      [selectedClass || '']: selectedPage === page,
+                    }}
+                    type="button"
+                    aria-label={`Page ${page} of ${totalPages}`}
+                    aria-current={selectedPage === page}
+                    onClick$={() => {
+                      onPageChange$(page);
+                    }}
+                  >
+                    {page}
+                  </button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          type="button"
+          aria-label={'nextAriaLabel'}
+          disabled={selectedPage >= totalPages}
+          onClick$={() => {
+            if (selectedPage < totalPages) {
+              onPageChange$(selectedPage + 1);
+            }
+          }}
+        >
+          <span>NEXT</span>
+          <div q:slot="suffix">➡️</div>
+        </button>
+      </nav>
     );
   },
 );
