ee60e18d90715017cdd6bcf2dfc32fbfd18cd3b2
maieulchevalier <maieul.chevalier@gmail.com>
2023-11-06 17:43:33 +0100

    refactor(/separator & /tabs): complete refactor

--- apps/website/src/examples/headless/tabs/dynamic.tsx
+++ apps/website/src/examples/headless/tabs/dynamic.tsx
@@ -1,31 +1,35 @@
-import { component$, useStore } from '@builder.io/qwik';
+import { component$, useStore, useStyles$ } from '@builder.io/qwik';
 import { Tab, TabList, TabPanel, Tabs } from '@qwik-ui/headless';
+import styles from './index.css?inline';
 
 export default component$(() => {
+  useStyles$(styles);
   const tabsState = useStore(['Dynamic Tab 1', 'Dynamic Tab 2', 'Dynamic Tab 3']);
   return (
     <>
-      <Tabs>
-        <TabList>
+      <div class="tabs-example mr-auto">
+        <Tabs>
+          <TabList>
+            {tabsState.map((tab) => (
+              <Tab key={tab}>{tab}</Tab>
+            ))}
+          </TabList>
           {tabsState.map((tab) => (
-            <Tab key={tab}>{tab}</Tab>
+            <TabPanel key={tab}>{tab} Panel</TabPanel>
           ))}
-        </TabList>
-        {tabsState.map((tab) => (
-          <TabPanel key={tab}>{tab} Panel</TabPanel>
-        ))}
-      </Tabs>
+        </Tabs>
 
-      <button
-        class="mt-4 font-bold text-red-600"
-        onClick$={() => {
-          if (tabsState.length > 1) {
-            tabsState.splice(0, 1);
-          }
-        }}
-      >
-        Remove First Tab
-      </button>
+        <button
+          class="mt-4 font-bold text-red-600"
+          onClick$={() => {
+            if (tabsState.length > 1) {
+              tabsState.splice(0, 1);
+            }
+          }}
+        >
+          Remove First Tab
+        </button>
+      </div>
     </>
   );
 });
--- apps/website/src/examples/headless/tabs/hero.tsx
+++ apps/website/src/examples/headless/tabs/hero.tsx
@@ -1,19 +1,18 @@
-import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
+import { component$, useStyles$ } from '@builder.io/qwik';
 import { Tab, TabList, TabPanel, Tabs } from '@qwik-ui/headless';
 import styles from './index.css?inline';
 
 export default component$(() => {
   useStyles$(styles);
-  const selectedTabIdSig = useSignal<string | undefined>();
   return (
     <>
       <div class="tabs-example mr-auto">
         <h3>Danish Composers</h3>
-        <Tabs bind:selectedTabId={selectedTabIdSig}>
+        <Tabs behavior="automatic">
           <TabList>
-            <Tab tabId="Maria">Maria Ahlefeldt</Tab>
-            <Tab tabId="Carl">Carl Andersen</Tab>
-            <Tab tabId="Ida">Ida Henriette da Fonseca</Tab>
+            <Tab>Maria Ahlefeldt</Tab>
+            <Tab>Carl Andersen</Tab>
+            <Tab>Ida Henriette da Fonseca</Tab>
           </TabList>
           <TabPanel>
             <p>Maria Theresia Ahlefeldt (16 January 1755 - 20 December 1810) was a ...</p>
@@ -25,9 +24,6 @@ export default component$(() => {
             <p>Ida Henriette da Fonseca (July 27, 1802 - July 6, 1858) was a ...</p>
           </TabPanel>
         </Tabs>
-        <p class="mt-4 text-white">
-          <strong>Selected Tab Id</strong>: {selectedTabIdSig.value}
-        </p>
       </div>
     </>
   );
