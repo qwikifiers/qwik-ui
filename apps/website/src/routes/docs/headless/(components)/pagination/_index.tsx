d0ac5ae6b9ff82923e8b4cf17b66e9217aaeae0f
maieulchevalier <maieul.chevalier@gmail.com>
2023-11-11 12:38:18 +0100

    fix(nx preview): fix pagination PR #492 that broke preview

--- apps/website/src/routes/docs/headless/(components)/pagination/_index.tsx
+++ apps/website/src/routes/docs/headless/(components)/pagination/_index.tsx
@@ -1,22 +1,22 @@
-import { component$, useStore } from '@builder.io/qwik';
-import { Pagination as HeadlessPagination } from '@qwik-ui/headless';
+import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
-  const store = useStore({
-    pages: 10,
-    page: 1,
-  });
+  // const store = useStore({
+  //   pages: 10,
+  //   page: 1,
+  // });
 
   return (
     <>
       <h2>This is the documentation for the Pagination</h2>
-      <HeadlessPagination
+      Pagination Example
+      {/* <HeadlessPagination
         pages={store.pages}
         page={store.page}
         onPaging$={(index) => {
           store.page = index;
         }}
-      />
+      /> */}
     </>
   );
 });
