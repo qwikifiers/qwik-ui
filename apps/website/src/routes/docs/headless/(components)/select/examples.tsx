6faf57ca163a5fdd20905635911dad4673da42f5
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-11-17 22:05:08 -0500

    docs(select/index.mdx): added draft docs

--- apps/website/src/routes/docs/headless/(components)/select/examples.tsx
+++ apps/website/src/routes/docs/headless/(components)/select/examples.tsx
@@ -16,7 +16,7 @@ export const Example01 = component$(() => {
     <PreviewCodeExampleTabsDeprecated>
       <div q:slot="actualComponent">
         <SelectRoot>
-          <SelectLabel class="ml-2 font-semibold text-white">Qwik Fruits</SelectLabel>
+          <SelectLabel class="ml-2 font-semibold text-white ">Qwik Fruits</SelectLabel>
           <SelectTrigger class="group peer flex items-center justify-between rounded-md border-[1px] border-slate-600 bg-slate-800 p-4 px-8">
             <SelectValue placeholder="Select a fruit! 🍹" class="text-white" />
             <SelectMarker class="h-6 w-6">
@@ -36,7 +36,7 @@ export const Example01 = component$(() => {
           <SelectListBox class="mt-2 rounded-md border-[1px] border-slate-600 bg-slate-800 text-white">
             <SelectOption
               optionValue="🚀 Qwik"
-              class="p-4 hover:bg-slate-700 focus:bg-slate-700"
+              class="p-4 hover:bg-slate-700 focus:bg-slate-700 "
             >
               🚀 Qwik
             </SelectOption>
@@ -110,6 +110,75 @@ export const Example02 = component$(() => {
               class="rounded-b-md p-4 hover:bg-slate-700 focus:bg-slate-700"
             >
               Contact us
+            </SelectOption>
+          </SelectListBox>
+        </SelectRoot>
+      </div>
+
+      <div q:slot="codeExample">
+        <Slot />
+      </div>
+    </PreviewCodeExampleTabsDeprecated>
+  );
+});
+
+export const Example03 = component$(() => {
+  return (
+    <PreviewCodeExampleTabsDeprecated>
+      <div q:slot="actualComponent">
+        <SelectRoot>
+          <SelectTrigger class="group peer flex items-center justify-between rounded-md border-[1px] border-slate-600 bg-slate-800 p-4 px-8">
+            <SelectValue placeholder="Pick a fruit" class="text-white" />
+            <SelectMarker class="h-6 w-6">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke-width="2"
+                class="stroke-white transition-transform duration-[450ms] group-aria-expanded:-rotate-180"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <polyline points="6 9 12 15 18 9"></polyline>
+              </svg>
+            </SelectMarker>
+          </SelectTrigger>
+          <SelectListBox class="mt-2 rounded-md border-[1px] border-slate-600 bg-slate-800 text-white">
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Apples
+            </SelectOption>
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Apricot
+            </SelectOption>
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Avocado
+            </SelectOption>
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Grapes
+            </SelectOption>
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Guava
+            </SelectOption>
+            <SelectOption
+              optionValue="Orders"
+              class="rounded-t-md p-4 hover:bg-slate-700 focus:bg-slate-700"
+            >
+              Oranges
             </SelectOption>
           </SelectListBox>
         </SelectRoot>
