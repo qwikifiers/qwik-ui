0707baf39592a82a045fbd214894feb66c513467
maieulchevalier <maieul.chevalier@gmail.com>
2023-11-23 10:59:21 +0100

    refactor(mdxprovider): add MDXProvider pattern

--- apps/website/src/routes/docs/headless/(components)/pagination/exports.ts
+++ apps/website/src/routes/docs/headless/(components)/pagination/exports.ts
@@ -1,6 +1,1 @@
-export { statusByComponent } from '~/_state/component-statuses';
-export { AnatomyTable } from '~/components/anatomy-table/anatomy-table';
-export { APITable } from '~/components/api-table/api-table';
-export { KeyboardInteractionTable } from '~/components/keyboard-interaction-table/keyboard-interaction-table';
-export { StatusBanner } from '~/components/status-banner/status-banner';
 export * from './examples';
