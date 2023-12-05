5bfe66f0d9c1f0eb6f0b0c2c3aaa4a78e38a7422
maieulchevalier <maieul.chevalier@gmail.com>
2023-12-05 16:39:31 +0100

    docs(css vars): add status-badge

--- apps/website/src/components/component-status-badge/component-status-badge.tsx
+++ apps/website/src/components/component-status-badge/component-status-badge.tsx
@@ -12,11 +12,11 @@ export function getClassByStatus(status: ComponentStatus) {
     case ComponentStatus.Ready:
       return 'text-green-900 bg-green-300 tracking-wide';
     case ComponentStatus.Beta:
-      return 'dark:text-slate-100 dark:bg-qwikui-purple-600 bg-qwikui-blue-600 text-white dark:border-qwikui-purple-600 border-[1px] border-qwikui-blue-700 tracking-wide shadow-dark-low dark:shadow-dark-low text-outline-beta';
+      return 'border border-primary tracking-wide bg-background text-foreground';
     case ComponentStatus.Draft:
-      return 'text-slate-200 bg-slate-700 dark:bg-slate-800 border-[1px] border-slate-400 shadow-dark-low tracking-wide dark:shadow-dark-low text-outline-draft';
+      return ' border  tracking-wide bg-background text-foreground';
     case ComponentStatus.Planned:
-      return 'text-white bg-slate-400 dark:bg-slate-600 border-[1px] border-slate-500 dark:border-slate-400 tracking-wide shadow-dark-low dark:shadow-dark-low text-outline-planned dark:text-outline-none';
+      return ' border tracking-wide bg-accent text-accent-foreground';
     default:
       return null;
   }
@@ -26,7 +26,7 @@ export const StatusBadge = component$<StatusBadgeProps>(({ status }) => {
   return (
     <Badge
       title={tooltipByStatus[status]}
-      class={`rounded-lg p-3 text-lg font-medium leading-3 lg:px-2 lg:py-1 lg:text-xs 
+      class={`rounded-lg p-3 text-lg font-medium leading-3 lg:px-2 lg:py-1 lg:text-xs
       ${getClassByStatus(status)}`}
     >
       {status}
