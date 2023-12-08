173270c2d7678173f2cbbb92a299a8486d2638ae
maieulchevalier <maieul.chevalier@gmail.com>
2023-12-08 08:01:32 +0100

    docs(modal): add backdrops

--- apps/website/src/routes/docs/headless/modal/examples/inspect.tsx
+++ apps/website/src/routes/docs/headless/modal/examples/inspect.tsx
@@ -14,7 +14,7 @@ export default component$(() => {
       </button>
       <Modal
         bind:show={showSig}
-        class="shadow-dark-medium bg-background text-foreground max-w-[25rem] rounded-md p-[28px]"
+        class="shadow-dark-medium bg-background text-foreground max-w-[25rem] rounded-md p-[28px] backdrop:backdrop-blur backdrop:backdrop-brightness-50 dark:backdrop:backdrop-brightness-100"
       >
         <ModalHeader>
           <h2 class="text-lg font-bold">Edit Profile</h2>
