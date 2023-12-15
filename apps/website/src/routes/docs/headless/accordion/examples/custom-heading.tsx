477f06be6a5982655bbb8e5516553e77e10c4968
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2023-12-14 19:21:48 -0500

    refactor(accordion/examples/custom-heading): tw slate-only colors & appropiate css changes

--- apps/website/src/routes/docs/headless/accordion/examples/custom-heading.tsx
+++ apps/website/src/routes/docs/headless/accordion/examples/custom-heading.tsx
@@ -11,10 +11,10 @@ export default component$(() => {
   return (
     <>
       <div class="flex w-full justify-center">
-        <AccordionRoot class="w-[min(400px,_100%)]">
-          <AccordionItem class="border-b">
+        <AccordionRoot class="w-[min(400px,_100%)] bg-slate-950 text-slate-50">
+          <AccordionItem class="border-b border-slate-950">
             <AccordionHeader as="h4">
-              <AccordionTrigger class="group flex w-full items-center justify-between rounded-t-sm py-4 text-left hover:underline">
+              <AccordionTrigger class="group flex w-full items-center justify-between rounded-t-sm bg-slate-700 p-4  text-left hover:underline">
                 <span>I'm an h4</span>
                 <span class="pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -24,12 +24,12 @@ export default component$(() => {
               </AccordionTrigger>
             </AccordionHeader>
             <AccordionContent>
-              <p class="pb-4">My Heading is an h4!</p>
+              <p class=" bg-slate-950 p-4">My Heading is an h4!</p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem class="border-b">
+          <AccordionItem class="border-b border-slate-950">
             <AccordionHeader as="h5">
-              <AccordionTrigger class="group flex w-full items-center justify-between py-4 text-left hover:underline">
+              <AccordionTrigger class="group flex w-full items-center justify-between bg-slate-700 p-4  text-left hover:underline">
                 <span>I'm an h5</span>
                 <span class="pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -39,12 +39,12 @@ export default component$(() => {
               </AccordionTrigger>
             </AccordionHeader>
             <AccordionContent>
-              <p class="pb-4">My Heading is an h5!</p>
+              <p class=" bg-slate-950 p-4">My Heading is an h5!</p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem class="border-b">
+          <AccordionItem class="border-b border-slate-950">
             <AccordionHeader as="h6">
-              <AccordionTrigger class="group flex w-full items-center justify-between py-4 text-left hover:underline aria-expanded:rounded-none">
+              <AccordionTrigger class="group flex w-full items-center justify-between bg-slate-700 p-4  text-left hover:underline aria-expanded:rounded-none">
                 <span>I'm an h6</span>
                 <span class="flex pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -54,7 +54,7 @@ export default component$(() => {
               </AccordionTrigger>
             </AccordionHeader>
             <AccordionContent>
-              <p class="pb-4">My Heading is an h6!</p>
+              <p class=" bg-slate-950 p-4">My Heading is an h6!</p>
             </AccordionContent>
           </AccordionItem>
         </AccordionRoot>
