d2ffda9305c442ac123e646068f1ac9a7ada025f
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2023-12-14 19:16:05 -0500

    refactor(accordion/examples/disabled): tw slate-only colors & appropiate css changes

--- apps/website/src/routes/docs/headless/accordion/examples/disabled.tsx
+++ apps/website/src/routes/docs/headless/accordion/examples/disabled.tsx
@@ -12,10 +12,10 @@ export default component$(() => {
   return (
     <>
       <div class="flex w-full justify-center">
-        <AccordionRoot class="w-[min(400px,_100%)]">
-          <AccordionItem class="border-b">
+        <AccordionRoot class="w-[min(400px,_100%)] bg-slate-950 text-slate-50">
+          <AccordionItem class="border-b border-slate-950">
             <AccordionHeader as="h3">
-              <AccordionTrigger class="group flex w-full items-center justify-between rounded-t-sm py-4 text-left hover:underline">
+              <AccordionTrigger class="group flex w-full items-center justify-between rounded-t-sm bg-slate-700 p-4  text-left hover:underline">
                 <span>I'm enabled!</span>
                 <span class="pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -25,15 +25,15 @@ export default component$(() => {
               </AccordionTrigger>
             </AccordionHeader>
             <AccordionContent>
-              <p class="pb-4">
+              <p class=" bg-slate-950 p-4">
                 Hey, I'm enabled! This is because I don't use the{' '}
                 <strong>disabled</strong> prop on the trigger.
               </p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem class="border-b">
+          <AccordionItem class="border-b border-slate-950">
             <AccordionHeader as="h3">
-              <AccordionTrigger class="group flex w-full items-center justify-between py-4 text-left hover:underline">
+              <AccordionTrigger class="group flex w-full items-center justify-between bg-slate-700 p-4  text-left hover:underline">
                 <span>I'm enabled!</span>
                 <span class="pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -43,23 +43,20 @@ export default component$(() => {
               </AccordionTrigger>
             </AccordionHeader>
             <AccordionContent>
-              <p class="pb-4">
+              <p class=" bg-slate-950 p-4">
                 Hey, I'm enabled! This is because I don't use the{' '}
                 <strong>disabled</strong> prop on the trigger.
               </p>
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem class="border-b">
+          <AccordionItem class="border-b border-slate-950 bg-slate-700">
             <h3>
               <AccordionTrigger
                 disabled
-                class="hover:bg-accent/50 group flex w-full items-center justify-between py-4 text-left aria-disabled:cursor-not-allowed aria-expanded:rounded-none"
+                class="group flex w-full items-center justify-between p-4 text-left  hover:bg-slate-500 aria-disabled:cursor-not-allowed aria-expanded:rounded-none"
               >
                 <span>
-                  I'm{' '}
-                  <span class="font-bold" style="color: red">
-                    disabled!
-                  </span>
+                  I'm <span class="bg-red-600 p-1 font-bold">disabled!</span>
                 </span>
                 <span class="flex pl-2">
                   <p class="scale-150 group-aria-expanded:rotate-45 group-aria-expanded:transform">
@@ -69,7 +66,7 @@ export default component$(() => {
               </AccordionTrigger>
             </h3>
             <AccordionContent>
-              <p class="pb-4">You shouldn't be able to see this!</p>
+              <p class=" bg-slate-950 p-4">You shouldn't be able to see this!</p>
             </AccordionContent>
           </AccordionItem>
         </AccordionRoot>
