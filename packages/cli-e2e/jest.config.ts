a1179a3cdfea6437d0d4475f858bb71f424b1d07
Shai Reznik <shairez@users.noreply.github.com>
2024-01-03 13:07:18 +0200

    chore: updated qwik version to 1.3.2

--- packages/cli-e2e/jest.config.ts
+++ packages/cli-e2e/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'qwik-ui-cli-e2e',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.cjs',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
