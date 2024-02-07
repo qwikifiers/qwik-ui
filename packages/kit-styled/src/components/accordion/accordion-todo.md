52e601dd280a1e58777a4db91c1c1639abea853c
Shai Reznik <shairez@users.noreply.github.com>
2024-02-07 03:13:42 +0200

    moved accordion todo  outside

---
+++ packages/kit-styled/src/components/accordion/accordion-todo.md
@@ -0,0 +1,7 @@
+## TODO
+
+- Implement 2 variants - "light", "bordered" (and "Splitted"?)
+- Implement shorthand version in headless and provide custom components ability
+- Ask Jack about the "Header" / "Trigger" separation
+
+RFC: Verbose API vs Simpler API
--- packages/kit-styled/src/components/accordion/accordion.tsx
+++ packages/kit-styled/src/components/accordion/accordion.tsx
@@ -15,16 +15,6 @@ import { cn } from '@qwik-ui/utils';
 
 import { LuChevronDown } from '@qwikest/icons/lucide';
 
-/* TODO:
-
-  * Implement 2 variants - "light", "bordered" (and "Splitted"?)
-  * Implement shorthand version in headless and provide custom components ability
-  * Ask Jack about the "Header" / "Trigger" separation
-
-  RFC: Verbose API vs Simpler API
-      
-*/
-
 const Accordion = component$<AccordionRootProps>((props) => (
   <QwikUIAccordionRoot animated {...props} class={props.class}>
     <Slot />
