425e62825e23d63b9b0dbcdaf7ae11d62481aa9b
Shai Reznik <shairez@users.noreply.github.com>
2024-02-22 03:16:05 +0200

    reverted nx upgrade
    Co-authored-by: Jack Shelton <thejackshelton@users.noreply.github.com>

--- packages/kit-fluffy/vite.config.ts
+++ packages/kit-fluffy/vite.config.ts
@@ -36,9 +36,6 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/packages/kit-fluffy',
-    reportCompressedSize: true,
-    commonjsOptions: { transformMixedEsModules: true },
     target: 'es2020',
     lib: {
       // Could also be a dictionary or array of multiple entry points.
@@ -56,7 +53,6 @@ export default defineConfig({
   },
   // @ts-ignore
   test: {
-    reporters: ['default'],
     globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
