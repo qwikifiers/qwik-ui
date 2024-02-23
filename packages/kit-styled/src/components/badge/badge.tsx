75341f7cabb8df7ac33f777ed337c9f8b6a9515a
maiieul <maieul.chevalier@gmail.com>
2024-02-23 19:20:02 +0100

    style(styled badge): refactor the way it's written but end result same

--- packages/kit-styled/src/components/badge/badge.tsx
+++ packages/kit-styled/src/components/badge/badge.tsx
@@ -3,16 +3,14 @@ import { cn } from '@qwik-ui/utils';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-base border px-2.5 py-0.5 text-xs font-semibold transition-colors shadow-sm focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-base px-2.5 py-0.5 text-xs font-semibold transition-colors shadow-sm focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       look: {
-        primary:
-          'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
-        secondary:
-          'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        alert: 'border-transparent bg-alert text-alert-foreground hover:bg-alert/80',
-        outline: 'text-foreground',
+        primary: 'bg-primary text-primary-foreground hover:bg-primary/80',
+        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        alert: 'bg-alert text-alert-foreground hover:bg-alert/80',
+        outline: 'border text-foreground',
       },
     },
     defaultVariants: {
