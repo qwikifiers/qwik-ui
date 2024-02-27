9aba3553d28799def76120a901a5ad1e27368f9b
maiieul <maieul.chevalier@gmail.com>
2024-02-27 15:02:29 +0100

    docs: fix CLS due to no fixed width on TOC

--- apps/website/src/routes/layout.tsx
+++ apps/website/src/routes/layout.tsx
@@ -39,7 +39,7 @@ export default component$(() => {
           <main class="w-full max-w-3xl px-4 py-8 sm:px-8">
             <Slot />
           </main>
-          <div class="hidden min-w-64 text-sm xl:block">
+          <div class="hidden w-64 text-sm xl:block">
             <div class="sticky top-16 -mt-10 mr-8 pt-4">
               <ScrollArea className="pb-10">
                 <div class="sticky top-16 -mt-10 h-[calc(100vh-64px)] py-12">
