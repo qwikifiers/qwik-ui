9e252da198d4492d364f69fd0d18f5f79f5d746e
thejackshelton <me@jackshelton.com>
2024-03-10 19:21:05 -0500

    refactor(headless): removed outdated components that use visible tasks

--- apps/website/src/routes/docs/headless/modal/snippets/page-load.tsx
+++ apps/website/src/routes/docs/headless/modal/snippets/page-load.tsx
@@ -1,15 +1,16 @@
-import { component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import { Modal, ModalContent, ModalFooter, ModalHeader } from '@qwik-ui/headless';
 
 export default component$(() => {
   const showSig = useSignal(false);
 
-  useVisibleTask$(
-    () => {
-      showSig.value = true;
-    },
-    { strategy: 'document-ready' },
-  );
+  // uncomment this
+  // useVisibleTask$(
+  //   () => {
+  //     showSig.value = true;
+  //   },
+  //   { strategy: 'document-ready' },
+  // );
 
   return (
     <Modal bind:show={showSig}>
--- packages/kit-headless/src/components/slider/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './slider';
-export * from './sliderProgress';
-export * from './sliderThumb';
