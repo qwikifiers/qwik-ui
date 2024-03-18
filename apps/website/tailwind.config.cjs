1c268cdf2437763e676764c7cc087c89e2c3debc
maiieul <maieul.chevalier@gmail.com>
2024-03-18 21:55:35 +0100

    fix(tailwind.config): add css var popover color

--- apps/website/tailwind.config.cjs
+++ apps/website/tailwind.config.cjs
@@ -85,6 +85,10 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
       },
       borderRadius: {
         base: 'var(--border-radius)',
