5501e88669c2310eb7a502c6cf277c21797e0e9c
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-03-23 16:24:37 -0400

    FEAT: working render of test

--- packages/kit-headless/src/components/checkbox/checkbox.test.ts
+++ packages/kit-headless/src/components/checkbox/checkbox.test.ts
@@ -1,11 +1,12 @@
 import AxeBuilder from '@axe-core/playwright';
 import { expect, test, type Page } from '@playwright/test';
 import { createTestDriver } from './checkbox.driver';
+import { renderToStream } from '@builder.io/qwik/server';
 
 async function setup(page: Page, exampleName: string) {
   await page.goto(`/headless/checkbox/${exampleName}`);
 
-  const driver = createTestDriver(page.getByRole('combobox'));
+  const driver = createTestDriver(page.getByRole('checkbox'));
 
   const { getRoot } = driver;
 
@@ -14,3 +15,13 @@ async function setup(page: Page, exampleName: string) {
     getRoot,
   };
 }
+
+test.describe.only('initial render', () => {
+  test(`GIVEN a checkbox with a user sig value of true
+        WHEN the checkbox renders
+        It should have the value of true`, async ({ page }) => {
+    const { driver: d } = await setup(page, 'hero');
+    console.log('hell');
+    await expect(d.getRoot()).toBeVisible();
+  });
+});
