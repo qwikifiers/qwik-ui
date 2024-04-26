d0595ec2eec05a097f41c2ed1f089f2ba9451700
jack shelton <me@jackshelton.com>
2024-04-26 10:46:12 -0500

    updated multi select examples

---
+++ apps/website/src/routes/docs/headless/select/examples/multiple-pill.tsx
@@ -0,0 +1,47 @@
+import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
+import {
+  Select,
+  SelectListbox,
+  SelectOption,
+  SelectPopover,
+  SelectTrigger,
+  SelectValue,
+} from '@qwik-ui/headless';
+import { LuX } from '@qwikest/icons/lucide';
+import styles from '../snippets/select.css?inline';
+
+export default component$(() => {
+  useStyles$(styles);
+  const users = ['Tim', 'Ryan', 'Jim', 'Jessie', 'Abby'];
+  const selected = useSignal<string[]>([]);
+
+  return (
+    <Select multiple bind:value={selected} class="select">
+      <SelectTrigger class="select-trigger">
+        <SelectValue>
+          {selected.value.map((opt) => (
+            <span class="select-pill" key={opt}>
+              {opt}
+              <span
+                onClick$={() =>
+                  (selected.value = selected.value.filter(
+                    (selectedOpt) => selectedOpt !== opt,
+                  ))
+                }
+              >
+                <LuX aria-hidden="true" />
+              </span>
+            </span>
+          ))}
+        </SelectValue>
+      </SelectTrigger>
+      <SelectPopover class="select-popover">
+        <SelectListbox class="select-listbox">
+          {users.map((user) => (
+            <SelectOption key={user}>{user}</SelectOption>
+          ))}
+        </SelectListbox>
+      </SelectPopover>
+    </Select>
+  );
+});
