b2a140f2b1466855bd8e66b794aaf67fd5b51a71
jack shelton <me@jackshelton.com>
2024-05-01 08:44:18 -0500

    fix: styled popover

--- packages/kit-styled/src/components/popover/popover.tsx
+++ packages/kit-styled/src/components/popover/popover.tsx
@@ -2,13 +2,18 @@ import { PropsOf, Slot, component$, useStyles$ } from '@builder.io/qwik';
 import { Popover } from '@qwik-ui/headless';
 import { cn } from '@qwik-ui/utils';
 
-export const PopoverRoot = Popover.Root;
+export const PopoverRoot = component$(() => {
+  return (
+    <Popover.Root gutter={8}>
+      <Slot />
+    </Popover.Root>
+  );
+});
 
 export const PopoverTrigger = Popover.Trigger;
 
-export const PopoverPanel = component$<PropsOf<typeof Popover.Panel>>(
-  ({ floating, ...props }) => {
-    useStyles$(`
+export const PopoverPanel = component$<PropsOf<typeof Popover.Panel>>(({ ...props }) => {
+  useStyles$(`
     .my-transition {
       transition: opacity 150ms, display 150ms, overlay 150ms;
       transition-behavior: allow-discrete;
@@ -23,18 +28,15 @@ export const PopoverPanel = component$<PropsOf<typeof Popover.Panel>>(
     }
     `);
 
-    return (
-      <Popover.Panel
-        {...props}
-        floating={floating}
-        class={cn(
-          'my-transition w-72 rounded-md border bg-popover p-4 text-popover-foreground opacity-0 shadow-md outline-none',
-          floating && 'absolute m-0',
-          props.class,
-        )}
-      >
-        <Slot />
-      </Popover.Panel>
-    );
-  },
-);
+  return (
+    <Popover.Panel
+      {...props}
+      class={cn(
+        'my-transition w-72 rounded-md border bg-popover p-4 text-popover-foreground opacity-0 shadow-md outline-none',
+        props.class,
+      )}
+    >
+      <Slot />
+    </Popover.Panel>
+  );
+});
