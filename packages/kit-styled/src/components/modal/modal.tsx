92424c89550df082c7d627944e7e81511a7909f7
maiieul <maieul.chevalier@gmail.com>
2024-05-16 23:46:28 +0200

    refactor(styled modal): to dot notation

--- packages/kit-styled/src/components/modal/modal.tsx
+++ packages/kit-styled/src/components/modal/modal.tsx
@@ -7,54 +7,61 @@ import {
   useContextProvider,
   useSignal,
 } from '@builder.io/qwik';
-import { Modal as QwikUIModal } from '@qwik-ui/headless';
+import { Modal as HeadlessModal } from '@qwik-ui/headless';
 import { cn } from '@qwik-ui/utils';
 
-export const ModalContext = createContextId<Signal<boolean>>('modal-context');
+const Context = createContextId<Signal<boolean>>('modal-context');
 
-export const ModalRoot = component$<PropsOf<typeof QwikUIModal.Root>>(({ ...props }) => {
+const Root = component$<PropsOf<typeof HeadlessModal.Root>>(({ ...props }) => {
   const showSig = useSignal(false);
-  useContextProvider(ModalContext, showSig);
+  useContextProvider(Context, showSig);
   return (
-    <QwikUIModal.Root bind:show={showSig} {...props}>
+    <HeadlessModal.Root bind:show={showSig} {...props}>
       <Slot />
-    </QwikUIModal.Root>
+    </HeadlessModal.Root>
   );
 });
 
-export const ModalPanel = component$(() => {
+const Panel = component$(() => {
   return (
-    <QwikUIModal.Panel class="max-w-sm rounded-base border border-white bg-background p-6 text-foreground shadow-md transition-all duration-300 backdrop:brightness-50 backdrop:backdrop-blur-sm backdrop:transition-all backdrop:duration-300 data-[state=open]:appear data-[state=closed]:disappear backdrop:data-[state=open]:appear backdrop:data-[state=closed]:disappear">
+    <HeadlessModal.Panel class="max-w-sm rounded-base border border-white bg-background p-6 text-foreground shadow-md transition-all duration-300 backdrop:brightness-50 backdrop:backdrop-blur-sm backdrop:transition-all backdrop:duration-300 data-[state=open]:appear data-[state=closed]:disappear backdrop:data-[state=open]:appear backdrop:data-[state=closed]:disappear">
       <Slot />
-    </QwikUIModal.Panel>
+    </HeadlessModal.Panel>
   );
 });
 
-export const ModalTrigger = component$<PropsOf<typeof QwikUIModal.Trigger>>(
-  ({ ...props }) => {
-    return (
-      <QwikUIModal.Trigger>
-        <Slot {...props} />
-      </QwikUIModal.Trigger>
-    );
-  },
-);
-
-export const ModalTitle = component$<PropsOf<'h2'>>(({ ...props }) => {
+const Trigger = component$<PropsOf<typeof HeadlessModal.Trigger>>(({ ...props }) => {
   return (
-    <QwikUIModal.Title
+    <HeadlessModal.Trigger>
+      <Slot {...props} />
+    </HeadlessModal.Trigger>
+  );
+});
+
+const Title = component$<PropsOf<'h2'>>(({ ...props }) => {
+  return (
+    <HeadlessModal.Title
       {...props}
       class={cn('flex flex-col space-y-1.5 text-center sm:text-left', props.class)}
     >
       <Slot />
-    </QwikUIModal.Title>
+    </HeadlessModal.Title>
   );
 });
 
-export const ModalDescription = component$<PropsOf<'p'>>(({ ...props }) => {
+const Description = component$<PropsOf<'p'>>(({ ...props }) => {
   return (
-    <QwikUIModal.Description {...props} class={cn('text-sm font-light', props.class)}>
+    <HeadlessModal.Description {...props} class={cn('text-sm font-light', props.class)}>
       <Slot />
-    </QwikUIModal.Description>
+    </HeadlessModal.Description>
   );
 });
+
+export const Modal = {
+  Root,
+  Panel,
+  Trigger,
+  Title,
+  Description,
+  Context,
+};
