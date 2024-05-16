1d44214694d643aec78184b790a53b1983f9287d
maiieul <maieul.chevalier@gmail.com>
2024-05-16 22:36:45 +0200

    refactor(styled breadcrumb): to dot notation

--- apps/website/src/components/ui/index.ts
+++ apps/website/src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from './lib/accordion';
 export * from './lib/alert';
 export * from './lib/avatar';
 export * from './lib/badge';
+export * from './lib/breadcrumb';
 export * from './lib/button';
 export * from './lib/checkbox';
 export * from './lib/input';
