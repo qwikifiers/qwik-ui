4c143f8fb91e450ab6867e6c20a734e918aa3a00
jack shelton <me@jackshelton.com>
2024-05-18 10:46:35 -0500

    docs: improve accordion examples

--- apps/website/src/routes/docs/headless/accordion/examples/animation.tsx
+++ apps/website/src/routes/docs/headless/accordion/examples/animation.tsx
@@ -1,29 +1,25 @@
-import { component$, useStyles$ } from '@builder.io/qwik';
+import { component$ } from '@builder.io/qwik';
 import { Accordion } from '@qwik-ui/headless';
 import { LuChevronDown } from '@qwikest/icons/lucide';
 
 export default component$(() => {
-  useStyles$(styles);
   const items = [1, 2, 3];
 
   return (
     <Accordion.Root>
       {items.map((item) => (
-        <Accordion.Item class="accordion-item" key={item}>
+        <Accordion.Item class="collapsible" key={item}>
           <Accordion.Header>
-            <Accordion.Trigger class="accordion-trigger">
+            <Accordion.Trigger class="collapsible-trigger">
               <span>Trigger {item}</span>
-              <LuChevronDown />
+              <LuChevronDown class="collapsible-transition" />
             </Accordion.Trigger>
           </Accordion.Header>
-          <Accordion.Content class="accordion-content accordion-animation">
-            Inside Content {item}
+          <Accordion.Content class="collapsible-animation collapsible-content">
+            <p class="collapsible-content-outline">Inside Content {item}</p>
           </Accordion.Content>
         </Accordion.Item>
       ))}
     </Accordion.Root>
   );
 });
-
-// interal
-import styles from '../snippets/accordion.css?inline';
