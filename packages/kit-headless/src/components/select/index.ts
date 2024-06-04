3054830f3a8c730551b13121c2fc7020d20e6e94
jack shelton <me@jackshelton.com>
2024-06-04 18:46:36 -0500

    export select description

--- packages/kit-headless/src/components/select/index.ts
+++ packages/kit-headless/src/components/select/index.ts
@@ -9,4 +9,5 @@ export { HSelectGroupLabel as GroupLabel } from './select-group-label';
 export { HSelectItem as Item } from './select-item';
 export { HSelectItemLabel as ItemLabel } from './select-item-label';
 export { HSelectItemIndicator as ItemIndicator } from './select-item-indicator';
+export { HSelectDescription as Description } from './select-description';
 export { HHiddenNativeSelect as HiddenNativeSelect } from './hidden-select';
