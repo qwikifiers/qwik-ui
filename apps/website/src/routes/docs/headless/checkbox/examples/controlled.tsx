6dcc66f6ba3b86f17bc16e7c26d6344c53d3dea9
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-06-07 17:48:42 -0400

    fix(docs/checkbox): finished documentation

--- apps/website/src/routes/docs/headless/checkbox/examples/controlled.tsx
+++ apps/website/src/routes/docs/headless/checkbox/examples/controlled.tsx
@@ -1,21 +1,23 @@
-import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import { Checkbox } from '@qwik-ui/headless';
 export default component$(() => {
-  const controlledSig = useSignal(false);
+  const initialVal1 = false;
+  const controlledSig1 = useSignal(initialVal1);
   return (
     <>
-      <div class="flex flex-col gap-3">
-        <Checkbox.Root
-          bind:checked={controlledSig}
-          id="test"
-          class="flex items-center gap-3 border-2 border-black p-2 "
-        >
-          <Checkbox.Indicator class="flex h-[25px] w-[25px] items-center justify-center bg-slate-600">
-            ✅
-          </Checkbox.Indicator>
-          Toggle Signal
-        </Checkbox.Root>
-        <p>Your signal's value is: {`${controlledSig.value}`}</p>
+      <div class="flex gap-8">
+        <div class="flex flex-col gap-3">
+          <Checkbox.Root
+            bind:checked={controlledSig1}
+            id="test"
+            class="flex items-center gap-3 border-2 border-black p-2 "
+          >
+            <Checkbox.Indicator class="flex h-[25px] w-[25px] items-center justify-center bg-slate-600">
+              ✅
+            </Checkbox.Indicator>
+            Toggle Value
+          </Checkbox.Root>
+        </div>
       </div>
     </>
   );
