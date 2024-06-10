eaa78f48d0c3bdf78250753febf891883fce3f97
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-06-10 16:48:21 -0400

    feat(automation): working regex-to-obj

--- packages/kit-headless/src/components/automation/commentMerger.js
+++ packages/kit-headless/src/components/automation/commentMerger.js
@@ -1,49 +1,21 @@
-import { log } from 'console';
 import * as fs from 'fs';
-myFileReader('../select/select-root.tsx');
-async function myFileReader(path) {
-  let esp_comment = '';
+myFileReader('../select/select-root.tsx', 'select-root');
+async function myFileReader(path, component_name) {
+  // we do what we can because we must
+  const magical_regex = /^\s*?\/[*]{2}\n?([\w|\W|]*?)\s*[*]{1,2}[/]\n[ ]*([\w|\W]*?;)$/gm;
   const cms = {};
   const sourceCode = fs.readFileSync(path, 'utf-8');
-  const component = /\/(\w+)\//.exec(path)[1];
-  // large arr should not be a problem since only the first lines of file will be passed down
-  const comments = sourceCode.toString().split('\n');
-  function pairer(index, line) {
-    return { desc: line, prop: comments[index + 1] };
-  }
-  for (let index = 0; index < comments.length; index++) {
-    const line = comments[index];
-    const singleLine = /^\s*\/[*]{2}(.*)[*]{1,2}\//;
-    const starter = /\/[*]{2}$/;
-    if (singleLine.test(line)) {
-      const hell_reloaded = singleLine.exec(line)[1];
-      if (cms.hasOwnProperty(component)) {
-        cms[component].push(pairer(index, line));
-        continue;
-      }
-      cms[component] = [pairer(index, line)];
-      continue;
-    }
-    if (starter.test(line)) {
-      index += 1;
-      let nextLine = comments[index];
-      let strg = '';
-      do {
-        nextLine = comments[index];
-        strg += nextLine;
-        index += 1;
-      } while (!/[*][/]$/.test(nextLine) | (index > comments.length));
-      strg = strg.replaceAll('\n', '');
+  const lol = magical_regex.exec(sourceCode);
+  let groups;
 
-      if (cms.hasOwnProperty(component)) {
-        cms[component].push({ desc: strg, prop: comments[index] });
-        continue;
-      }
-      cms[component] = [pairer(index, line)];
+  while ((groups = magical_regex.exec(sourceCode)) !== null) {
+    const comment = groups[1];
+    const prop = groups[2];
+    if (cms.hasOwnProperty(component_name)) {
+      cms[component_name].push({ comment, prop });
       continue;
     }
+    cms[component_name] = [{ comment, prop }];
   }
   console.log(cms);
 }
-const magical_regex = /^\s*?\/[*]{2}\n?([\w|\W|]*?)\s*[*]{1,2}[/]\n[ ]*([\w|\W]*?;)$/gm;
-// TODO: fix case of multiple comment but start is not white space
