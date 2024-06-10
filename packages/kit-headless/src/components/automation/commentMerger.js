a70a44daa7c8e141d97a35fb3bdc913581f523ee
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-06-10 00:20:19 -0400

    feat(regex): and now it respect white space

--- packages/kit-headless/src/components/automation/commentMerger.js
+++ packages/kit-headless/src/components/automation/commentMerger.js
@@ -45,5 +45,5 @@ async function myFileReader(path) {
   }
   console.log(cms);
 }
-const magical_regex = /^\s*\/[*]{2}([\w|\W|]*?)[*]{1,2}[/]\n([\w|\W]*?;)$/gm;
+const magical_regex = /^\s*?\/[*]{2}\n?([\w|\W|]*?)\s*[*]{1,2}[/]\n[ ]*([\w|\W]*?;)$/gm;
 // TODO: fix case of multiple comment but start is not white space
