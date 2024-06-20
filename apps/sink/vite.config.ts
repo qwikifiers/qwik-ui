eb063122795c2ec4336a075e072d2fbf168ab923
maiieul <maieul.chevalier@gmail.com>
2024-06-20 23:19:41 +0200

    feat(pkg.pr.new): add sink app with tailwind

---
+++ apps/sink/vite.config.ts
@@ -0,0 +1,42 @@
+import { qwikVite } from "@builder.io/qwik/optimizer";
+import { qwikCity } from "@builder.io/qwik-city/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { qwikNxVite } from "qwik-nx/plugins";
+
+export default defineConfig({
+  cacheDir: "../../node_modules/.vite/apps/sink",
+  plugins: [
+    qwikNxVite(),
+    qwikCity(),
+    qwikVite({
+      client: {
+        outDir: "../../dist/apps/sink/client",
+      },
+      ssr: {
+        outDir: "../../dist/apps/sink/server",
+      },
+      tsconfigFileNames: ["tsconfig.app.json"],
+    }),
+    tsconfigPaths({ root: "../../" }),
+  ],
+  server: {
+    fs: {
+      // Allow serving files from the project root
+      allow: ["../../"],
+    },
+  },
+  preview: {
+    headers: {
+      "Cache-Control": "public, max-age=600",
+    },
+  },
+  test: {
+    globals: true,
+    cache: {
+      dir: "../../node_modules/.vitest",
+    },
+    environment: "node",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
+});
