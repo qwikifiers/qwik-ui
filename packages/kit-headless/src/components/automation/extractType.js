7699370517e6acda6ab12a053f72e53865a2ce4e
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-06-20 15:12:42 -0400

    refactor(extractType): splits regex from file handle

--- packages/kit-headless/src/components/automation/extractType.js
+++ packages/kit-headless/src/components/automation/extractType.js
@@ -1,11 +1,21 @@
 import * as fs from 'fs';
 import * as util from 'util';
 async function myFileReader(path, component_name, output) {
+  const sourceCode = fs.readFileSync(path, 'utf-8');
+  const cms = getPublicTypes(component_name, sourceCode);
+  const strg = `export const output=${util.inspect(cms)}`;
+  try {
+    fs.writeFileSync(output, strg);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export function getPublicTypes(component_name, strg) {
   const getPublicTypes = /type Public.*?{([\w|\W]*?)};/gm;
   const cms = {};
-  const sourceCode = fs.readFileSync(path, 'utf-8');
   let groups;
-  while ((groups = getPublicTypes.exec(sourceCode)) !== null) {
+  while ((groups = getPublicTypes.exec(strg)) !== null) {
     const trimStart = /^ *|(\* *)/g;
     const comments = groups[1].replaceAll(trimStart, '');
     if (cms.hasOwnProperty(component_name)) {
@@ -14,11 +24,6 @@ async function myFileReader(path, component_name, output) {
     }
     cms[component_name] = [{ comments }];
   }
-  const strg = `export const output=${util.inspect(cms)}`;
-  try {
-    fs.writeFileSync(output, strg);
-  } catch (err) {
-    console.error(err);
-  }
+  return cms;
 }
 myFileReader('../select/select-root.tsx', 'select-root', './output.js');
