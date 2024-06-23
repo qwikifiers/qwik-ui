6748a45f4a4831662f9a19ae08ac3d486626c297
jack shelton <me@jackshelton.com>
2024-06-23 11:07:56 -0500

    onInput$ and filter docs

--- apps/website/src/routes/docs/headless/combobox/examples/input.tsx
+++ apps/website/src/routes/docs/headless/combobox/examples/input.tsx
@@ -5,6 +5,7 @@ import { LuCheck, LuChevronDown } from '@qwikest/icons/lucide';
 export default component$(() => {
   useStyles$(styles);
   const count = useSignal(0);
+  const inputValue = useSignal('');
 
   const fruits = [
     'Apple',
@@ -17,20 +18,24 @@ export default component$(() => {
     'Coconut',
   ];
 
-  const handleInput$ = $(() => {
+  const handleInput$ = $((value: string) => {
     count.value++;
+    inputValue.value = value;
   });
 
   return (
     <>
       <Combobox.Root class="combobox-root" onInput$={handleInput$}>
+        <div>onInput$ value: {inputValue.value}</div>
         <Combobox.Label class="combobox-label">Personal Trainers</Combobox.Label>
+
         <Combobox.Control class="combobox-control">
           <Combobox.Input class="combobox-input" />
           <Combobox.Trigger class="combobox-trigger">
             <LuChevronDown class="combobox-icon" />
           </Combobox.Trigger>
         </Combobox.Control>
+
         <Combobox.Popover class="combobox-popover" gutter={8}>
           {fruits.map((fruit) => (
             <Combobox.Item key={fruit} class="combobox-item">
