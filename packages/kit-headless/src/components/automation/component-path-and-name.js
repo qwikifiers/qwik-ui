55cead97424cf3b923f8b320fd1354aa5963bf01
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-06-26 00:51:51 -0400

    refactor(automatic api): better file name and ouput

--- packages/kit-headless/src/components/automation/component-path-and-name.js
+++ packages/kit-headless/src/components/automation/component-path-and-name.js
@@ -1,13 +1,14 @@
 import { readdirSync } from 'fs';
-export function objectifier() {
+export function objectifier(path) {
   const jsx = {};
-  const files = readdirSync('..', { recursive: true });
+  const files = readdirSync(path, { recursive: true });
   for (let index = 0; index < files.length; index++) {
     const file = files[index];
     const isJSX = /.*tsx/.test(file);
 
     if (isJSX) {
       const title = /(.*)[/]/.exec(file)[1];
+      getPathAndName(file);
       if (!jsx.hasOwnProperty(title)) {
         jsx[title] = [file];
         continue;
@@ -16,3 +17,12 @@ export function objectifier() {
     }
   }
 }
+
+function getPathAndName(strg) {
+  const title = /(.*)[/]/.exec(strg)[1];
+  const subTitle = /\/(.*)\.tsx/.exec(strg)[1];
+  if (subTitle.includes('-')) {
+  }
+  console.log(title, subTitle, strg);
+}
+objectifier('..');
