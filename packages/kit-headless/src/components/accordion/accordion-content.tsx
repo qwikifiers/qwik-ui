79424b094018446926fb8a9993e04c6ffac91cf9
Jack Shelton <104264123+thejackshelton@users.noreply.github.com>
2024-07-04 13:28:31 -0500

    Revert "my cvommit"
    This reverts commit fed7c2f43bfd89f9eca19cc449bb496a6f615466.

--- packages/kit-headless/src/components/accordion/accordion-content.tsx
+++ packages/kit-headless/src/components/accordion/accordion-content.tsx
@@ -1,8 +1,6 @@
 import { PropsOf, Slot, component$ } from '@builder.io/qwik';
 import { HCollapsibleContent } from '../collapsible/collapsible-content';
 
-/// here's a commit
-
 export const HAccordionContent = component$(
   (props: PropsOf<typeof HCollapsibleContent>) => {
     return (
