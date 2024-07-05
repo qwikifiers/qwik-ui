4061837008ac4475479ec2530745edddbc6ee363
jack shelton <me@jackshelton.com>
2024-07-05 11:44:04 -0500

    remove refs in hero example

--- apps/website/src/routes/docs/headless/combobox/examples/hero.tsx
+++ apps/website/src/routes/docs/headless/combobox/examples/hero.tsx
@@ -1,25 +1,9 @@
-import { component$, useSignal, useStyles$, useVisibleTask$ } from '@builder.io/qwik';
+import { component$, useStyles$ } from '@builder.io/qwik';
 import { Combobox } from '@qwik-ui/headless';
 import { LuCheck, LuChevronDown } from '@qwikest/icons/lucide';
 
 export default component$(() => {
   useStyles$(styles);
-
-  const rootRef = useSignal<HTMLDivElement>();
-  const triggerRef = useSignal<HTMLButtonElement>();
-  const inputRef = useSignal<HTMLInputElement>();
-  const labelRef = useSignal<HTMLDivElement>();
-  const controlRef = useSignal<HTMLDivElement>();
-  const popoverRef = useSignal<HTMLDivElement>();
-
-  useVisibleTask$(() => {
-    console.log('rootRef', rootRef.value);
-    console.log('triggerRef', triggerRef.value);
-    console.log('inputRef', inputRef.value);
-    console.log('labelRef', labelRef.value);
-    console.log('controlRef', controlRef.value);
-    console.log('popoverRef', popoverRef.value);
-  });
 
   const fruits = [
     'Apple',
@@ -33,17 +17,15 @@ export default component$(() => {
   ];
 
   return (
-    <Combobox.Root class="combobox-root" ref={rootRef}>
-      <Combobox.Label class="combobox-label" ref={labelRef}>
-        Personal Trainers
-      </Combobox.Label>
-      <Combobox.Control class="combobox-control" ref={controlRef}>
-        <Combobox.Input class="combobox-input" ref={inputRef} />
-        <Combobox.Trigger class="combobox-trigger" ref={triggerRef}>
+    <Combobox.Root class="combobox-root">
+      <Combobox.Label class="combobox-label">Personal Trainers</Combobox.Label>
+      <Combobox.Control class="combobox-control">
+        <Combobox.Input class="combobox-input" />
+        <Combobox.Trigger class="combobox-trigger">
           <LuChevronDown class="combobox-icon" />
         </Combobox.Trigger>
       </Combobox.Control>
-      <Combobox.Popover class="combobox-popover" gutter={8} ref={popoverRef}>
+      <Combobox.Popover class="combobox-popover" gutter={8}>
         {fruits.map((fruit) => (
           <Combobox.Item key={fruit} class="combobox-item">
             <Combobox.ItemLabel>{fruit}</Combobox.ItemLabel>
