49dc6b881544a2653651ba4ae5a14daf7c588165
jack shelton <me@jackshelton.com>
2024-07-19 00:07:28 -0500

    small docs

--- packages/kit-headless/src/components/carousel/pagination.tsx
+++ packages/kit-headless/src/components/carousel/pagination.tsx
@@ -1,9 +1,5 @@
 import { type PropsOf, component$, Slot } from '@builder.io/qwik';
 
-/* Why we use the range util: 
-https://www.joshwcomeau.com/snippets/javascript/range/
-*/
-
 type CarouselPaginationProps = PropsOf<'div'>;
 
 export const CarouselPagination = component$(({ ...props }: CarouselPaginationProps) => {
