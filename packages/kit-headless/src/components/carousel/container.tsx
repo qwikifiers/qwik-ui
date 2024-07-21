e2090ecd57a278b05f3406a057fd4ce5da052c72
jack shelton <me@jackshelton.com>
2024-07-20 20:10:42 -0500

    use scorllLeft instead

--- packages/kit-headless/src/components/carousel/container.tsx
+++ packages/kit-headless/src/components/carousel/container.tsx
@@ -1,4 +1,11 @@
-import { component$, type PropsOf, Slot, useContext } from '@builder.io/qwik';
+import {
+  component$,
+  type PropsOf,
+  Slot,
+  useContext,
+  useSignal,
+  $,
+} from '@builder.io/qwik';
 import { carouselContextId } from './context';
 import { useStyles$ } from '@builder.io/qwik';
 import styles from './carousel.css?inline';
@@ -8,17 +15,46 @@ type CarouselContainerProps = PropsOf<'div'>;
 export const CarouselContainer = component$((props: CarouselContainerProps) => {
   const context = useContext(carouselContextId);
   useStyles$(styles);
+  const isMouseDownSig = useSignal(false);
+  const startXSig = useSignal();
+  const scrollLeftSig = useSignal(0);
+
+  const handleMouseMove$ = $((e: MouseEvent) => {
+    if (!isMouseDownSig.value) return;
+    const x = e.pageX - context.containerRef.value.offsetLeft;
+    console.log('x', x);
+    const SCROLL_SPEED = 3;
+    const walk = (x - startXSig.value) * SCROLL_SPEED;
+    console.log('walk', walk);
+    context.containerRef.value.scrollLeft = scrollLeftSig.value - walk;
+
+    console.log('carousel container', context.containerRef.value);
+
+    console.log('scroll left', context.containerRef.value.scrollLeft);
+    console.log('move');
+  });
+
+  const handleMouseDown$ = $((e: MouseEvent) => {
+    if (!context.isDraggableSig.value) return;
+    isMouseDownSig.value = true;
+    startXSig.value = e.pageX - context.containerRef.value.offsetLeft;
+    scrollLeftSig.value = context.containerRef.value.scrollLeft;
+    window.addEventListener('mousemove', handleMouseMove$);
+  });
+
+  const handleWindowMouseUp$ = $(() => {
+    isMouseDownSig.value = false;
+    window.removeEventListener('mousemove', handleMouseMove$);
+  });
 
   return (
     <div
       ref={context.containerRef}
-      style={{
-        transform: `translate3d(${context.slideOffsetSig.value}px, 0px, 0px)`,
-        transitionDuration: `${context.transitionDurationSig.value}ms`,
-        transitionDelay: '0ms',
-      }}
+      onMouseDown$={[handleMouseDown$, props.onMouseDown$]}
+      window:onMouseUp$={[handleWindowMouseUp$, props['window:onPointerUp$']]}
       data-draggable={context.isDraggableSig.value ? '' : undefined}
       data-qui-carousel-container
+      preventdefault:mousemove
       {...props}
     >
       <Slot />
--- packages/kit-headless/src/components/carousel/viewport.tsx
+++ packages/kit-headless/src/components/carousel/viewport.tsx
@@ -1,4 +1,4 @@
-import { component$, type PropsOf, Slot, useContext, $ } from '@builder.io/qwik';
+import { component$, type PropsOf, Slot, useContext } from '@builder.io/qwik';
 import { carouselContextId } from './context';
 
 type CarouselViewportProps = PropsOf<'div'>;
@@ -6,110 +6,8 @@ type CarouselViewportProps = PropsOf<'div'>;
 export const CarouselView = component$((props: CarouselViewportProps) => {
   const context = useContext(carouselContextId);
 
-  const handlePointerMove$ = $((e: PointerEvent) => {
-    if (context.isMouseDraggingSig.value) {
-      if (!context.containerRef.value) {
-        return;
-      }
-
-      context.transitionDurationSig.value = 0;
-
-      const deltaX = e.clientX - context.initialX.value;
-
-      // const containerWidth = context.containerRef.value.scrollWidth;
-      const containerLeftOffset = context.initialTransformX.value + deltaX;
-
-      if (containerLeftOffset > 50) {
-        return;
-      }
-
-      // TODO: fix bug where pointer down before the animation ends (slow move)
-
-      context.slideOffsetSig.value = containerLeftOffset;
-
-      /* --- */
-
-      /* TODO: Optimize this by checking prev and next of current slides first */
-      for (let i = 0; i < context.slideRefsArray.value.length; i++) {
-        const slideRef = context.slideRefsArray.value[i];
-
-        if (!context.containerRef.value || !slideRef.value) {
-          return;
-        }
-
-        /*
-            TODO: figure out why a separate DOMMatrix is why dragging and the buttons work properly.
-          */
-        const style = window.getComputedStyle(context.containerRef.value);
-        const matrix = new DOMMatrix(style.transform);
-
-        const containerTranslateX = matrix.m41;
-        // How far to the left the slides container is shifted.
-        const absContainerTranslateX = Math.abs(containerTranslateX);
-
-        if (!context.viewportRef.value) {
-          return;
-        }
-
-        // How far the left edge of this slide is from the left of the slides container.
-        const slideSlideContainerLeftOffset = slideRef.value.offsetLeft;
-        // How far the right edge of this slide is from the left of the slides container
-        // (includes space between slide).
-        const slideRightEdgePos =
-          slideSlideContainerLeftOffset +
-          slideRef.value.offsetWidth +
-          context.spaceBetweenSlides;
-
-        const carouselViewportWidth = context.viewportRef.value.offsetWidth;
-        const halfViewportWidth = carouselViewportWidth / 2;
-
-        const isWithinBounds =
-          absContainerTranslateX > slideSlideContainerLeftOffset - halfViewportWidth &&
-          absContainerTranslateX < slideRightEdgePos - halfViewportWidth;
-
-        if (isWithinBounds) {
-          context.currentIndexSig.value = i || 0;
-          break;
-        }
-      }
-    }
-  });
-
-  const handlePointerDown$ = $((e: PointerEvent) => {
-    if (!context.isDraggableSig.value) return;
-
-    // check if the device is a pointer device
-    if (e.pointerType !== 'mouse' && e.pointerType !== 'pen') {
-      return;
-    }
-
-    context.initialX.value = e.clientX;
-    if (context.containerRef.value) {
-      const style = window.getComputedStyle(context.containerRef.value);
-      const matrix = new DOMMatrix(style.transform);
-      context.initialTransformX.value = matrix.m41;
-
-      context.isMouseDraggingSig.value = true;
-    }
-
-    window.addEventListener('pointermove', handlePointerMove$);
-  });
-
-  const handleWindowPointerUp$ = $(() => {
-    /* removes pointer move event from pointer up created in slide.tsx */
-    window.removeEventListener('pointermove', handlePointerMove$);
-  });
-
-  const handleTransitionEnd$ = $(() => (context.transitionDurationSig.value = 0));
-
   return (
-    <div
-      ref={context.viewportRef}
-      onPointerDown$={[handlePointerDown$, props.onPointerDown$]}
-      window:onPointerUp$={[handleWindowPointerUp$, props['window:onPointerUp$']]}
-      onTransitionEnd$={[handleTransitionEnd$, props.onTransitionEnd$]}
-      {...props}
-    >
+    <div ref={context.viewportRef} {...props}>
       <Slot />
     </div>
   );
