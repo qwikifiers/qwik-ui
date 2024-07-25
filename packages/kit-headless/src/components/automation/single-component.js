96a28e4e58b7ba106c5dd897945a42cf976b6581
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-07-25 18:58:30 -0400

    feat(auto api): dynamically gets component name

--- packages/kit-headless/src/components/automation/single-component.js
+++ packages/kit-headless/src/components/automation/single-component.js
@@ -3,14 +3,16 @@ import * as util from 'util';
 import { extractPublicTypes } from '../automation/extract-type.js';
 import { extractComments } from './extract-comment.js';
 myFileReader('../select/select-root.tsx', 'select-root', './output.js');
-async function myFileReader(path, component_name, output) {
+async function myFileReader(path, output) {
+  const component_name = /\/([\w-]*).tsx/.exec(path);
   const sourceCode = fs.readFileSync(path, 'utf-8');
   const comments = extractPublicTypes(component_name, sourceCode);
-  const total = [];
+  const parsed = [];
   for (const comment of comments) {
     const api = extractComments([comment.label], comment.string);
     const pair = { [comment.label]: api };
-    total.push(pair);
+    parsed.push(pair);
   }
-  console.log(util.inspect(total, false, 8));
+  const componentAPI = { [component_name[1]]: parsed };
+  console.log(util.inspect(componentAPI, false, 8));
 }
