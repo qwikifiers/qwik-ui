86bc5a220c7bb35d005f625385b4a7f82fad770a
jack shelton <me@jackshelton.com>
2024-07-30 14:23:57 -0500

    allow people to pass handlers to modal close

--- packages/kit-headless/src/components/modal/modal-close.tsx
+++ packages/kit-headless/src/components/modal/modal-close.tsx
@@ -1,11 +1,15 @@
-import { PropsOf, Slot, component$, useContext } from '@builder.io/qwik';
+import { PropsOf, Slot, component$, useContext, $ } from '@builder.io/qwik';
 import { modalContextId } from './modal-context';
 
 export const HModalClose = component$((props: PropsOf<'button'>) => {
   const context = useContext(modalContextId);
 
+  const handleClick$ = $(() => {
+    context.showSig.value = false;
+  });
+
   return (
-    <button onClick$={() => (context.showSig.value = false)} {...props}>
+    <button onClick$={[handleClick$, props.onClick$]} {...props}>
       <Slot />
     </button>
   );
