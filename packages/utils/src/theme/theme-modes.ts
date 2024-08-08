927c2d4117ffe9c07fc0c75b9df412d5662ad6c1
Shai Reznik <shairez@users.noreply.github.com>
2024-08-08 16:08:07 +0300

    Fix CLI (#914)
    * added nx/workspace-data to gitignore
    
    * utils: changed enums to consts
    
    * fix cli types

---
+++ packages/utils/src/theme/theme-modes.ts
@@ -0,0 +1,8 @@
+import { ObjectValues } from '../type-utils';
+
+export const ThemeModes = {
+  LIGHT: 'light',
+  DARK: 'dark',
+} as const;
+
+export type ThemeMode = ObjectValues<typeof ThemeModes>;
