620d1d749b20beb771b51720308a772202a49892
maiieul <maieul.chevalier@gmail.com>
2024-08-13 04:47:56 +0200

    chore(website): charSet -> charset in root.tsx

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -43,7 +43,7 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
+        <meta charset="utf-8" />
         <link rel="manifest" href="/manifest.json" />
         <RouterHead />
         <ServiceWorkerRegister />
