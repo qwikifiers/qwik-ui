af6b0e59a003566798d4bf17e0f20633d5af185c
ArkadiK94 <arkadi29081994@gmail.com>
2024-08-23 21:21:25 +0300

    docs: add the vertical-direction section to docs
    why: so users could see that it is possible to make it vertical

---
+++ apps/website/src/routes/docs/headless/carousel/examples/vertical-direction.tsx
@@ -0,0 +1,27 @@
+import { component$, useStyles$ } from '@builder.io/qwik';
+import { Carousel } from '@qwik-ui/headless';
+
+export default component$(() => {
+  useStyles$(styles);
+
+  const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange', 'pink'];
+
+  return (
+    <Carousel.Root class="carousel-root" gap={30} direction="column" maxSlideHeight={160}>
+      <div class="carousel-buttons">
+        <Carousel.Previous>Down</Carousel.Previous>
+        <Carousel.Next>Up</Carousel.Next>
+      </div>
+      <Carousel.Scroller class="carousel-scroller">
+        {colors.map((color) => (
+          <Carousel.Slide key={color} class="carousel-slide">
+            {color}
+          </Carousel.Slide>
+        ))}
+      </Carousel.Scroller>
+    </Carousel.Root>
+  );
+});
+
+// internal
+import styles from './carousel.css?inline';
