d4948dc96ca7b182777d0ba2a39ef961a781067c
steff <steffanek@gmail.com>
2024-09-12 22:36:30 +0200

    docs: remove default from styled toggle

--- apps/website/src/routes/docs/styled/toggle/examples/initialPressed.tsx
+++ apps/website/src/routes/docs/styled/toggle/examples/initialPressed.tsx
@@ -2,5 +2,5 @@ import { component$ } from '@builder.io/qwik';
 import { Toggle } from '~/components/ui';
 
 export default component$(() => {
-  return <Toggle defaultPressed={true}>Hello</Toggle>;
+  return <Toggle pressed={true}>Hello</Toggle>;
 });
