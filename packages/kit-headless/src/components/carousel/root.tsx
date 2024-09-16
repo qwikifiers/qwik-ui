f27dbe699f09ed28b8d974ed69f5e1e56cff611c
jack shelton <me@jackshelton.com>
2024-09-16 08:48:11 -0500

    fix: text align

--- packages/kit-headless/src/components/carousel/root.tsx
+++ packages/kit-headless/src/components/carousel/root.tsx
@@ -75,128 +75,130 @@ export type CarouselRootProps = PropsOf<'div'> & {
   maxSlideHeight?: number;
 };
 
-export const CarouselBase = component$(
-  ({
+export const CarouselBase = component$((props: CarouselRootProps) => {
+  const {
+    align,
     'bind:currSlideIndex': givenOldSlideIndexSig,
     'bind:selectedIndex': givenSlideIndexSig,
     'bind:autoplay': givenAutoplaySig,
     'bind:progress': givenProgressSig,
     _isTitle: isTitle,
     startIndex,
-    ...props
-  }: CarouselRootProps) => {
-    // core state
-    const localId = useId();
-    const scrollerRef = useSignal<HTMLDivElement>();
-    const nextButtonRef = useSignal<HTMLButtonElement>();
-    const prevButtonRef = useSignal<HTMLButtonElement>();
-    const scrollStartRef = useSignal<HTMLDivElement>();
-    const isMouseDraggingSig = useSignal<boolean>(false);
-    const slideRefsArray = useSignal<Array<Signal>>([]);
-    const bulletRefsArray = useSignal<Array<Signal>>([]);
-    const startIndexSig = useComputed$(() => {
-      return startIndex ?? givenSlideIndexSig?.value ?? 0;
-    });
-    const currentIndexSig = useBoundSignal(
-      givenSlideIndexSig ?? givenOldSlideIndexSig,
-      startIndexSig.value,
-    );
-    const isScrollerSig = useSignal(false);
-    const isAutoplaySig = useBoundSignal(givenAutoplaySig, false);
+    ...rest
+  } = props;
 
-    const getInitialProgress = () => {
-      return startIndexSig.value
-        ? startIndexSig.value / ((props._numSlides ?? 1) - 1)
-        : 0;
+  /* we don't want to pass the align attribute to the carousel-root */
+  align;
+
+  // core state
+  const localId = useId();
+  const scrollerRef = useSignal<HTMLDivElement>();
+  const nextButtonRef = useSignal<HTMLButtonElement>();
+  const prevButtonRef = useSignal<HTMLButtonElement>();
+  const scrollStartRef = useSignal<HTMLDivElement>();
+  const isMouseDraggingSig = useSignal<boolean>(false);
+  const slideRefsArray = useSignal<Array<Signal>>([]);
+  const bulletRefsArray = useSignal<Array<Signal>>([]);
+  const startIndexSig = useComputed$(() => {
+    return startIndex ?? givenSlideIndexSig?.value ?? 0;
+  });
+  const currentIndexSig = useBoundSignal(
+    givenSlideIndexSig ?? givenOldSlideIndexSig,
+    startIndexSig.value,
+  );
+  const isScrollerSig = useSignal(false);
+  const isAutoplaySig = useBoundSignal(givenAutoplaySig, false);
+
+  const getInitialProgress = () => {
+    return startIndexSig.value ? startIndexSig.value / ((props._numSlides ?? 1) - 1) : 0;
+  };
+
+  // derived
+  const numSlidesSig = useComputed$(() => props._numSlides ?? 0);
+  const isDraggableSig = useComputed$(() => props.draggable ?? true);
+  const slidesPerViewSig = useComputed$(() => props.slidesPerView ?? 1);
+  const gapSig = useComputed$(() => props.gap ?? 0);
+  const alignSig = useComputed$(() => props.align ?? 'start');
+  const isLoopSig = useComputed$(() => props.loop ?? false);
+  const autoPlayIntervalMsSig = useComputed$(() => props.autoPlayIntervalMs ?? 0);
+  const progressSig = useBoundSignal(givenProgressSig, getInitialProgress());
+  const sensitivitySig = useComputed$(() => {
+    return {
+      mouse: props.sensitivity?.mouse ?? 1.5,
+      touch: props.sensitivity?.touch ?? 1.25,
     };
+  });
+  const moveSig = useComputed$(() => props.move ?? 1);
+  const maxSlideHeightSig = useComputed$(() => props.maxSlideHeight ?? undefined);
+  const orientationSig = useComputed$(() => {
+    if (props.maxSlideHeight === undefined) {
+      return 'horizontal';
+    }
+    return props.orientation ?? 'horizontal';
+  });
 
-    // derived
-    const numSlidesSig = useComputed$(() => props._numSlides ?? 0);
-    const isDraggableSig = useComputed$(() => props.draggable ?? true);
-    const slidesPerViewSig = useComputed$(() => props.slidesPerView ?? 1);
-    const gapSig = useComputed$(() => props.gap ?? 0);
-    const alignSig = useComputed$(() => props.align ?? 'start');
-    const isLoopSig = useComputed$(() => props.loop ?? false);
-    const autoPlayIntervalMsSig = useComputed$(() => props.autoPlayIntervalMs ?? 0);
-    const progressSig = useBoundSignal(givenProgressSig, getInitialProgress());
-    const sensitivitySig = useComputed$(() => {
-      return {
-        mouse: props.sensitivity?.mouse ?? 1.5,
-        touch: props.sensitivity?.touch ?? 1.25,
-      };
-    });
-    const moveSig = useComputed$(() => props.move ?? 1);
-    const maxSlideHeightSig = useComputed$(() => props.maxSlideHeight ?? undefined);
-    const orientationSig = useComputed$(() => {
-      if (props.maxSlideHeight === undefined) {
-        return 'horizontal';
-      }
-      return props.orientation ?? 'horizontal';
-    });
+  const titleId = `${localId}-title`;
 
-    const titleId = `${localId}-title`;
+  const context: CarouselContext = {
+    localId,
+    scrollerRef,
+    nextButtonRef,
+    prevButtonRef,
+    scrollStartRef,
+    isMouseDraggingSig,
+    slideRefsArray,
+    bulletRefsArray,
+    currentIndexSig,
+    isScrollerSig,
+    isAutoplaySig,
+    numSlidesSig,
+    isDraggableSig,
+    slidesPerViewSig,
+    gapSig,
+    alignSig,
+    isLoopSig,
+    autoPlayIntervalMsSig,
+    startIndexSig,
+    sensitivitySig,
+    moveSig,
+    orientationSig,
+    maxSlideHeightSig,
+  };
 
-    const context: CarouselContext = {
-      localId,
-      scrollerRef,
-      nextButtonRef,
-      prevButtonRef,
-      scrollStartRef,
-      isMouseDraggingSig,
-      slideRefsArray,
-      bulletRefsArray,
-      currentIndexSig,
-      isScrollerSig,
-      isAutoplaySig,
-      numSlidesSig,
-      isDraggableSig,
-      slidesPerViewSig,
-      gapSig,
-      alignSig,
-      isLoopSig,
-      autoPlayIntervalMsSig,
-      startIndexSig,
-      sensitivitySig,
-      moveSig,
-      orientationSig,
-      maxSlideHeightSig,
-    };
+  useAutoplay(context);
 
-    useAutoplay(context);
+  useContextProvider(carouselContextId, context);
 
-    useContextProvider(carouselContextId, context);
+  useTask$(({ track }) => {
+    if (!givenProgressSig) return;
+    track(() => currentIndexSig.value);
+    track(() => numSlidesSig.value);
 
-    useTask$(({ track }) => {
-      if (!givenProgressSig) return;
-      track(() => currentIndexSig.value);
-      track(() => numSlidesSig.value);
+    if (numSlidesSig.value > 1) {
+      progressSig.value = (currentIndexSig.value / (numSlidesSig.value - 1)) * 100;
+    } else {
+      progressSig.value = 0;
+    }
+  });
 
-      if (numSlidesSig.value > 1) {
-        progressSig.value = (currentIndexSig.value / (numSlidesSig.value - 1)) * 100;
-      } else {
-        progressSig.value = 0;
-      }
-    });
-
-    return (
-      <div
-        role="group"
-        aria-labelledby={isTitle ? titleId : undefined}
-        aria-label={!isTitle ? `content slideshow` : undefined}
-        aria-roledescription="carousel"
-        aria-live={isAutoplaySig.value ? 'off' : 'polite'}
-        data-qui-carousel
-        {...props}
-        style={{
-          '--slides-per-view': slidesPerViewSig.value,
-          '--gap': `${gapSig.value}px`,
-          '--scroll-snap-align': alignSig.value,
-          '--orientation': orientationSig.value === 'vertical' ? 'column' : 'row',
-          '--max-slide-height': `${maxSlideHeightSig.value}px`,
-        }}
-      >
-        <Slot />
-      </div>
-    );
-  },
-);
+  return (
+    <div
+      role="group"
+      aria-labelledby={isTitle ? titleId : undefined}
+      aria-label={!isTitle ? `content slideshow` : undefined}
+      aria-roledescription="carousel"
+      aria-live={isAutoplaySig.value ? 'off' : 'polite'}
+      data-qui-carousel
+      {...rest}
+      style={{
+        '--slides-per-view': slidesPerViewSig.value,
+        '--gap': `${gapSig.value}px`,
+        '--scroll-snap-align': alignSig.value,
+        '--orientation': orientationSig.value === 'vertical' ? 'column' : 'row',
+        '--max-slide-height': `${maxSlideHeightSig.value}px`,
+      }}
+    >
+      <Slot />
+    </div>
+  );
+});
