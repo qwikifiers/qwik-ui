a219d8fe55d4d6d2c60882e6d97e53a7f7dda0e8
Jerry_wu <409187100@qq.com>
2024-09-19 19:24:23 +0800

    init debug

---
+++ apps/website/src/routes/docs/styled/switch/examples/hero.tsx
@@ -0,0 +1,12 @@
+import { component$ } from '@builder.io/qwik';
+import { Switch } from '~/components/ui';
+
+
+export default component$(() => {
+  return (
+    <>
+    sdsdsd
+      <Switch />
+    </>
+  );
+});
---
+++ packages/kit-styled/src/components/switch/switch.tsx
@@ -0,0 +1,13 @@
+import { type PropsOf, component$ } from '@builder.io/qwik';
+import { cn } from '@qwik-ui/utils';
+
+
+export const Switch = component$<PropsOf<'input'>>(({ ...props }) => {
+  return (
+    <input
+      type="checkbox"
+      {...props}
+      class={cn('h-4 w-4 accent-primary disabled:cursor-not-allowed disabled:opacity-50', props.class)}
+    />
+  );
+});
