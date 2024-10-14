70976c6f02e5cd7d9a56c864a83098b254c0613b
jack shelton <me@jackshelton.com>
2024-10-14 10:56:16 -0500

    fix: qrl's inside computed

---
+++ apps/website/src/routes/docs/headless/combobox/auto-api/api.ts
@@ -0,0 +1,85 @@
+export const api = {
+  combobox: [
+    {
+      'combobox-context': [],
+    },
+    {
+      'combobox-control': [],
+    },
+    {
+      'combobox-description': [],
+    },
+    {
+      'combobox-empty': [],
+    },
+    {
+      'combobox-error-message': [],
+    },
+    {
+      'combobox-group-label': [],
+    },
+    {
+      'combobox-group': [],
+    },
+    {
+      'combobox-hidden-option': [],
+    },
+    {
+      'combobox-hidden-select': [],
+    },
+    {
+      'combobox-inline': [],
+    },
+    {
+      'combobox-input': [],
+    },
+    {
+      'combobox-item-indicator': [],
+    },
+    {
+      'combobox-item-label': [],
+    },
+    {
+      'combobox-item': [
+        {
+          HComboboxItemProps: [
+            {
+              comment:
+                ' Internal index we get from the inline component. Please see combobox-inline.tsx ',
+              prop: '_index',
+              type: 'number',
+            },
+            {
+              comment: ' If true, item is not selectable or focusable. ',
+              prop: 'disabled',
+              type: 'boolean',
+            },
+            {
+              comment: ' Selected value associated with the item. ',
+              prop: 'value',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      'combobox-label': [],
+    },
+    {
+      'combobox-listbox': [],
+    },
+    {
+      'combobox-popover': [],
+    },
+    {
+      'combobox-root': [],
+    },
+    {
+      'combobox-trigger': [],
+    },
+    {
+      'use-combobox': [],
+    },
+  ],
+};
