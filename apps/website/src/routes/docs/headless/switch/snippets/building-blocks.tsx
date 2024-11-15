c62dddaa7640068efe74adc4dd10162e3df258d0
Jerry_wu <409187100@qq.com>
2024-11-15 11:31:12 +0800

    fix switch

---
+++ apps/website/src/routes/docs/headless/switch/snippets/building-blocks.tsx
@@ -0,0 +1,21 @@
+import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
+import { Switch } from '@qwik-ui/headless';
+
+
+export default component$(() => {
+  const checked = useSignal(false)
+  useStyles$(styles);
+
+  return (
+    <Switch.Root
+      bind:checked={checked}
+    >
+      <Switch.Label>test</Switch.Label>
+      <Switch.Input />
+    </Switch.Root>
+  );
+});
+
+import styles from '../snippets/switch.css?inline';
+
+
