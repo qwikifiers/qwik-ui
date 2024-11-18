a132016cee872209c5a00a557392eb311a152227
Jerry_wu <409187100@qq.com>
2024-11-18 11:13:29 +0800

    css

--- packages/kit-styled/src/components/switch/switch.tsx
+++ packages/kit-styled/src/components/switch/switch.tsx
@@ -26,7 +26,20 @@ const Label = component$<PropsOf<typeof HeadlessSwitch.Label>>(({ ...props }) =>
 });
 
 const Input = component$<PropsOf<typeof HeadlessSwitch.Input>>(({ ...props }) => {
-  return <HeadlessSwitch.Input {...props} />;
+  return (
+    <HeadlessSwitch.Input
+      {...props}
+      class={cn(
+        `class="p-2px bg-inactive inline-size[4rem] block-size[2rem] rounded-full
+        appearance-none pointer-events-none touch-pan-y border-none outline-offset[5px]
+        box-content flex-shrink-0 grid place-items-center grid-cols-[track] transition-bg[.25s]
+        ease before:content-[""] before:cursor-pointer before:pointer-events-auto before:grid-area-[track]
+        before:inline-size[2rem] before:block-size[2rem] before:bg-thumb before:box-shadow[0_0_0_0_hsla(var(--switch-thumb-color-highlight))]
+        before:rounded-[50%] before:transform[translateX(0%)]`,
+        props.class,
+      )}
+    />
+  );
 });
 
 
