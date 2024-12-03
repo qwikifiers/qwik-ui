db37e7311d6d50f77afb76edce3355f7beb7bd98
Jerry_wu <409187100@qq.com>
2024-12-03 10:48:43 +0800

    switch

--- packages/kit-styled/src/components/switch/switch.tsx
+++ packages/kit-styled/src/components/switch/switch.tsx
@@ -7,7 +7,7 @@ const Root = component$<PropsOf<typeof HeadlessSwitch.Root>>(({ ...props }) => {
     <HeadlessSwitch.Root
       {...props}
       class={cn(
-        'tap-highlight-transparent flex cursor-pointer select-none flex-row-reverse items-center justify-between gap-1',
+        'tap-highlight-transparent flex cursor-pointer select-none flex-row-reverse items-center justify-between gap-[8px]',
         props.class,
       )}
     >
@@ -29,7 +29,18 @@ const Input = component$<PropsOf<typeof HeadlessSwitch.Input>>(({ ...props }) =>
     <HeadlessSwitch.Input
       {...props}
       class={cn(
-        'bg-switchInactive transition-bg[.25s] ease before:content-[" "] pointer-events-none box-content grid flex-shrink-0 touch-pan-y appearance-none grid-cols-[1fr] grid-rows-[1fr] rounded-full border-none p-[4px] block-size-8 inline-size-16 before:pointer-events-auto before:translate-x-[0%] before:cursor-pointer before:rounded-[50%] before:bg-background before:block-size-8 before:inline-size-8',
+        `bg-switchInactive transition-bg[.25s] ease before:content-[" "]
+        pointer-events-none box-content grid flex-shrink-0 touch-pan-y
+        appearance-none grid-cols-[1fr] grid-rows-[1fr] rounded-full
+        border-none p-[4px] block-size-8 inline-size-16 before:pointer-events-auto
+        before:translate-x-[0%] before:cursor-pointer before:rounded-[50%]
+        before:bg-background before:block-size-8 before:inline-size-8
+        enabled:hover:before:shadow-[0_0_0_0.5rem_hsla(var(--switch-thumb-color-highlight))]
+        disabled:cursor-not-allowed disabled:opacity-35 disabled:before:cursor-not-allowed
+        disabled:before:box-shadow-[inset_0_0_0_2px_hsl(0_0%_100%_/_10%)]
+        checked:bg-primary checked:before:translate-x-[100%] focus:outline-2 focus:outline-primary
+        focus:outline-offset-2,
+        `,
         props.class,
       )}
     />
