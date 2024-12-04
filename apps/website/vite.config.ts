14d28da57627c76d71d8aa23ab8b6998d7d11bf3
maiieul <maieul.chevalier@gmail.com>
2024-12-04 21:00:26 +0800

    chore: remove recma plugin

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -2,7 +2,6 @@ import { qwikRouter } from '@qwik.dev/router/vite';
 import { qwikVite } from '@qwik.dev/core/optimizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { recmaProvideComponents } from './recma-provide-components';
 import autoAPI from './auto-api';
 
 export default defineConfig(async () => {
@@ -39,7 +38,7 @@ export default defineConfig(async () => {
         },
         mdx: {
           providerImportSource: '~/_state/MDXProvider',
-          recmaPlugins: [recmaProvideComponents],
+          // recmaPlugins: [recmaProvideComponents],
           rehypePlugins: [
             () => (tree) => {
               visit(tree, (node) => {
