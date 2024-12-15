2fc1aeb251dbab998704becdce0f6dc0fb9672f0
jack shelton <me@jackshelton.com>
2024-12-15 17:05:41 -0600

    styles

--- apps/website/src/components/search/search.tsx
+++ apps/website/src/components/search/search.tsx
@@ -1,10 +1,21 @@
 import { component$ } from '@builder.io/qwik';
 import { Combobox, Modal } from '@qwik-ui/headless';
+import { buttonVariants } from '@qwik-ui/styled';
+import { cn } from '@qwik-ui/utils';
+import { LuSearch } from '@qwikest/icons/lucide';
 
 export const SearchModal = component$(() => {
   return (
     <Modal.Root>
-      <Modal.Trigger class="sha border border-foreground">Search</Modal.Trigger>
+      <Modal.Trigger
+        class={cn(
+          buttonVariants({ size: 'sm', look: 'outline' }),
+          'flex h-8 items-center gap-2 sm:h-10',
+        )}
+      >
+        <LuSearch class="h-4 w-4" />
+        <span class="hidden sm:block">Search</span>
+      </Modal.Trigger>
       <Modal.Panel>
         <Search />
       </Modal.Panel>
