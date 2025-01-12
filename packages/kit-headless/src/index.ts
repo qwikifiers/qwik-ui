6e257ecffd31fef3e087c60b1cda69df0f95edb2
maiieul <maieul.chevalier@gmail.com>
2025-01-12 11:33:38 +0100

    refactor: headless accordion without dot syntax

--- packages/kit-headless/src/index.ts
+++ packages/kit-headless/src/index.ts
@@ -1,6 +1,6 @@
 export * as Checkbox from './components/checkbox';
 export * as Checklist from './components/checklist';
-export * as Accordion from './components/accordion/';
+export * from './components/accordion/';
 export * as Carousel from './components/carousel';
 export * as Collapsible from './components/collapsible';
 export * from './components/label';
