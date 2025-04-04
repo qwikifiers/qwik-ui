89f141e76057ea78c5de4c55306178f3a93d61fa
maiieul <maieul.chevalier@rosa.be>
2025-04-04 13:43:20 +0200

    chore: modulepreload 200

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -36,7 +36,7 @@ export default component$(() => {
   useContextProvider(APP_STATE_CONTEXT_ID, appState);
 
   return (
-    <QwikCityProvider prefetchLimit={50}>
+    <QwikCityProvider prefetchLimit={200}>
       <head>
         <meta charset="utf-8" />
         <link rel="manifest" href="/manifest.json" />
