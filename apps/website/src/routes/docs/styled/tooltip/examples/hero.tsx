bd26d01b8327fe049ec13ffd812b135589b44909
maiieul <maieul.chevalier@rosa.be>
2025-06-03 12:19:39 +0200

    chore: nx v21 migration

--- apps/website/src/routes/docs/styled/tooltip/examples/hero.tsx
+++ apps/website/src/routes/docs/styled/tooltip/examples/hero.tsx
@@ -5,10 +5,10 @@ export default component$(() => {
   return (
     <Tooltip.Root flip={false} gutter={8}>
       <Tooltip.Trigger class={badgeVariants({ look: 'outline' })}>
-        Hover over me.
+        Hover over me 👀
       </Tooltip.Trigger>
       <Tooltip.Panel>
-        <p>Add to library</p>
+        <p>I'm a tooltip</p>
       </Tooltip.Panel>
     </Tooltip.Root>
   );
--- packages/kit-headless/src/components/carousel/title.tsx
+++ packages/kit-headless/src/components/carousel/title.tsx
@@ -1,8 +1,8 @@
-import { component$, Slot, useContext } from '@builder.io/qwik';
+import { component$, PropsOf, Slot, useContext } from '@builder.io/qwik';
 import { carouselContextId } from './context';
 
 /** Used to distinguish accessible label from other carousels */
-export const CarouselTitle = component$(() => {
+export const CarouselTitle = component$<PropsOf<'div'>>(() => {
   const context = useContext(carouselContextId);
   const titleId = `${context.localId}-title`;
 
