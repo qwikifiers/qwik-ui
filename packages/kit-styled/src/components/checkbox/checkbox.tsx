684187da37a55a9a1de509b022ff8c691a6ec139
maiieul <maieul.chevalier@rosa.be>
2025-08-06 17:48:15 +0200

    fix(styled kit): allow consumer to pass onInput$ with two way data-binding & fix forms

--- packages/kit-styled/src/components/checkbox/checkbox.tsx
+++ packages/kit-styled/src/components/checkbox/checkbox.tsx
@@ -2,16 +2,16 @@ import { $, type PropsOf, component$ } from '@builder.io/qwik';
 import { cn } from '@qwik-ui/utils';
 
 export const Checkbox = component$<Partial<PropsOf<'input'> & { type?: 'checkbox' }>>(
-  ({ id, name, ['bind:checked']: checkedSig, checked, onInput$, ...props }) => {
+  ({ id, name, ['bind:checked']: bindSig, checked, onInput$, ...props }) => {
     const inputId = id || name;
     return (
       <input
         {...props}
         type="checkbox"
         // workaround to support two way data-binding on the Input component (https://github.com/QwikDev/qwik/issues/3926)
-        checked={checkedSig ? checkedSig.value : checked}
-        onInput$={checkedSig ? $((_, el) => (checkedSig.value = el.checked)) : onInput$}
-        data-checked={checked || checkedSig?.value || ''}
+        checked={bindSig ? bindSig.value : checked}
+        onInput$={[bindSig && $((_, el) => (bindSig.value = el.checked)), onInput$]}
+        data-checked={checked || bindSig?.value || ''}
         class={cn(
           'peer h-4 w-4 shrink-0 border-primary text-primary accent-primary ring-offset-background focus:ring-ring focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
           props.class,
