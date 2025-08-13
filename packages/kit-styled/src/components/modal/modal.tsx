75cd2289d0f18b2e00d647fefcd42a7922a5c49e
maiieul <maieul.chevalier@rosa.be>
2025-08-13 15:42:21 +0200

    fix: modal is no longer centered by default

--- packages/kit-styled/src/components/modal/modal.tsx
+++ packages/kit-styled/src/components/modal/modal.tsx
@@ -11,21 +11,21 @@ const Close = HeadlessModal.Close;
 
 export const panelVariants = cva(
   [
-    'bg-background text-foreground fixed p-6 transition-all backdrop:brightness-50 backdrop:backdrop-blur-xs',
-    'data-open:animate-in data-closing:animate-out data-closing:duration-300 data-open:duration-300',
-    'data-open:backdrop:animate-in data-closing:backdrop:animate-out data-closing:backdrop:fade-out data-open:backdrop:fade-in data-closing:backdrop:duration-300 data-open:backdrop:duration-300',
+    'fixed bg-background p-6 text-foreground transition-all backdrop:brightness-50 backdrop:backdrop-blur-xs',
+    'data-closing:animate-out data-closing:duration-300 data-open:animate-in data-open:duration-300',
+    'data-closing:backdrop:animate-out data-closing:backdrop:duration-300 data-closing:backdrop:fade-out data-open:backdrop:animate-in data-open:backdrop:duration-300 data-open:backdrop:fade-in',
   ],
   {
     variants: {
       position: {
         center:
-          'rounded-base data-closed:fade-out data-open:fade-in data-closed:zoom-out-95 data-open:zoom-in-95 data-open:slide-in-from-bottom-2 data-closing:backdrop:fade-out data-open:backdrop:fade-in max-w-lg shadow-lg',
-        top: 'rounded-b-base data-closing:slide-out-to-top data-open:slide-in-from-top inset-x-0 top-0 mt-0 mb-auto border-b',
+          'rounded-base m-auto max-w-lg shadow-lg data-closed:zoom-out-95 data-closed:fade-out data-closing:backdrop:fade-out data-open:zoom-in-95 data-open:fade-in data-open:slide-in-from-bottom-2 data-open:backdrop:fade-in',
+        top: 'rounded-b-base inset-x-0 top-0 mb-auto w-full border-b data-closing:slide-out-to-top data-open:slide-in-from-top',
         bottom:
-          'rounded-t-base data-closing:slide-out-to-bottom data-open:slide-in-from-bottom inset-x-0 bottom-0 mt-auto mb-0 border-t',
-        left: 'rounded-r-base data-closing:slide-out-to-left data-open:slide-in-from-left inset-y-0 left-0 mr-auto ml-0 h-full max-w-sm border-r',
+          'rounded-t-base inset-x-0 bottom-0 mt-auto w-full border-t data-closing:slide-out-to-bottom data-open:slide-in-from-bottom',
+        left: 'rounded-r-base inset-y-0 left-0 mr-auto h-full max-w-sm border-r data-closing:slide-out-to-left data-open:slide-in-from-left',
         right:
-          'rounded-l-base data-closing:slide-out-to-right data-open:slide-in-from-right inset-y-0 right-0 mr-0 ml-auto h-full max-w-sm border-l',
+          'rounded-l-base inset-y-0 right-0 ml-auto h-full max-w-sm border-l data-closing:slide-out-to-right data-open:slide-in-from-right',
       },
     },
     defaultVariants: {
@@ -60,7 +60,7 @@ const Description = component$<PropsOf<'p'>>(({ ...props }) => {
   return (
     <HeadlessModal.Description
       {...props}
-      class={cn('text-muted-foreground text-sm', props.class)}
+      class={cn('text-sm text-muted-foreground', props.class)}
     >
       <Slot />
     </HeadlessModal.Description>
